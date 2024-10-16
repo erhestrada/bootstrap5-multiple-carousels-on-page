f0a28058fe8adb23f20de275f1fc3e213ab95b76
Erik <180784081+erhetrada@users.noreply.github.com>
2024-10-16 00:31:22 -0400

    move appropriate code from getTopClips into makeClipsCarouselFromClipsData

--- getTopClips.js
+++ getTopClips.js
@@ -113,112 +113,110 @@ export async function getTopClips(clientId, authToken, game, daysBack, broadcast
         }
       });
       const clipsData = await response.json();
-      const embedUrls = clipsData.data.map((datum) => datum.embed_url);
-      localStorage.setItem("embedUrls", JSON.stringify(embedUrls));
-      embedUrls.forEach((element, index) => {localStorage.setItem(index, element)});
-      const thumbnailUrls = clipsData.data.map((datum) => datum.thumbnail_url);
-      const titles = clipsData.data.map((datum) => datum.title);
-      const languages = clipsData.data.map((datum) => datum.language);
-      const viewCounts = clipsData.data.map((datum) => datum.view_count);
-      const streamers = clipsData.data.map((datum) => datum.broadcaster_name);
-      const streamerIds = clipsData.data.map((datum) => datum.broadcaster_id);
-      const creationDateTimes = clipsData.data.map((datum) => datum.created_at);
-      const durations = clipsData.data.map((datum) => datum.duration);
-
-      updateDonutPfp(streamerIds[0]);
-
-      const popularClipsCarouselInner = document.getElementById('popular-clips-carousel-inner');
-
-      thumbnailUrls.forEach((url, index) => {
-        // checking for english should happen higher up - that's why i'm getting non english clips in my main carousel
-        if(languages[index] === 'en') {
-
-
-          const carouselItem = document.createElement('div');
-          carouselItem.id = 'popular-clips' + index;
-          carouselItem.className = "carousel-item"
-          // Including this makes first thumbnail snap to top of container, included because bootstrap carousel supposedly needs a .active item, but everything's working fine without it
-          /*
-          if (index === 0) {
-            carouselItem.classList.add('active');
-          }*/
-
-          const card = document.createElement('div');
-          card.className = "card";
-
-          const imageWrapper = document.createElement('div');
-          imageWrapper.className = "img-wrapper";
-          imageWrapper.id = "img-wrapper" + index;
-
-          // formerly thumbnail
-          const image = document.createElement('img');
-          image.src = url + "&parent=localhost";
-          image.classList.add('thumbnail');
-          image.addEventListener('click', () => {thumbnailClickListener(index, embedUrls, streamerIds)});
-          image.addEventListener('click', () => {highlightDiv(imageWrapper)});
-
-          const cardBody = document.createElement('div');
-          cardBody.className = 'card-body';
-
-          /*
-          const cardTitle = document.createElement('h1');
-          cardTitle.innerText = 'Card Title'
-          cardTitle.style.color = "#6441A4"
-          */
-
-          const clipTitle = document.createElement('p');
-          clipTitle.innerText = titles[index];
-          clipTitle.style.color = "#FFFFFF";
-
-          const viewCount = document.createElement('p');
-          viewCount.innerText = viewCounts[index].toLocaleString() + ' views';
-          viewCount.style.color = "#FFFFFF";
-
-          const streamer = document.createElement('p');
-          streamer.innerText = streamers[index];
-          streamer.style.color = "#FFFFFF";
-
-          const creationDate = document.createElement('p');
-          creationDate.innerText = creationDateTimes[index];
-          creationDate.style.color = "#FFFFFF";
-
-          const duration = document.createElement('p');
-          duration.innerText = Math.round(durations[index]) + 's';
-          duration.style.color = "#FFFFFF";
-
-          popularClipsCarouselInner.appendChild(carouselItem);
-          carouselItem.appendChild(card);
-          
-          card.appendChild(imageWrapper);
-          imageWrapper.appendChild(image);
-          card.appendChild(cardBody);
-          //cardBody.appendChild(cardTitle);
-          cardBody.appendChild(clipTitle);
-          cardBody.appendChild(viewCount);
-          cardBody.appendChild(streamer);
-          cardBody.appendChild(creationDate);
-          cardBody.appendChild(duration);
-
-
-          /*
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          */
-
-          
-        }
-
-      });
-  
+      makeClipsCarouselFromClipsData(clipsData);
       return clipsData;
     } catch (error) {
       console.error(error);
     }
   }
 
-function makeClipsCarouselFromClipsData() {
+function makeClipsCarouselFromClipsData(clipsData) {
+  const embedUrls = clipsData.data.map((datum) => datum.embed_url);
+  localStorage.setItem("embedUrls", JSON.stringify(embedUrls));
+  embedUrls.forEach((element, index) => {localStorage.setItem(index, element)});
+  const thumbnailUrls = clipsData.data.map((datum) => datum.thumbnail_url);
+  const titles = clipsData.data.map((datum) => datum.title);
+  const languages = clipsData.data.map((datum) => datum.language);
+  const viewCounts = clipsData.data.map((datum) => datum.view_count);
+  const streamers = clipsData.data.map((datum) => datum.broadcaster_name);
+  const streamerIds = clipsData.data.map((datum) => datum.broadcaster_id);
+  const creationDateTimes = clipsData.data.map((datum) => datum.created_at);
+  const durations = clipsData.data.map((datum) => datum.duration);
 
+  updateDonutPfp(streamerIds[0]);
+  const popularClipsCarouselInner = document.getElementById('popular-clips-carousel-inner');
+
+  thumbnailUrls.forEach((url, index) => {
+    // checking for english should happen higher up - that's why i'm getting non english clips in my main carousel
+    if(languages[index] === 'en') {
+
+
+      const carouselItem = document.createElement('div');
+      carouselItem.id = 'popular-clips' + index;
+      carouselItem.className = "carousel-item"
+      // Including this makes first thumbnail snap to top of container, included because bootstrap carousel supposedly needs a .active item, but everything's working fine without it
+      /*
+      if (index === 0) {
+        carouselItem.classList.add('active');
+      }*/
+
+      const card = document.createElement('div');
+      card.className = "card";
+
+      const imageWrapper = document.createElement('div');
+      imageWrapper.className = "img-wrapper";
+      imageWrapper.id = "img-wrapper" + index;
+
+      // formerly thumbnail
+      const image = document.createElement('img');
+      image.src = url + "&parent=localhost";
+      image.classList.add('thumbnail');
+      image.addEventListener('click', () => {thumbnailClickListener(index, embedUrls, streamerIds)});
+      image.addEventListener('click', () => {highlightDiv(imageWrapper)});
+
+      const cardBody = document.createElement('div');
+      cardBody.className = 'card-body';
+
+      /*
+      const cardTitle = document.createElement('h1');
+      cardTitle.innerText = 'Card Title'
+      cardTitle.style.color = "#6441A4"
+      */
+
+      const clipTitle = document.createElement('p');
+      clipTitle.innerText = titles[index];
+      clipTitle.style.color = "#FFFFFF";
+
+      const viewCount = document.createElement('p');
+      viewCount.innerText = viewCounts[index].toLocaleString() + ' views';
+      viewCount.style.color = "#FFFFFF";
+
+      const streamer = document.createElement('p');
+      streamer.innerText = streamers[index];
+      streamer.style.color = "#FFFFFF";
+
+      const creationDate = document.createElement('p');
+      creationDate.innerText = creationDateTimes[index];
+      creationDate.style.color = "#FFFFFF";
+
+      const duration = document.createElement('p');
+      duration.innerText = Math.round(durations[index]) + 's';
+      duration.style.color = "#FFFFFF";
+
+      popularClipsCarouselInner.appendChild(carouselItem);
+      carouselItem.appendChild(card);
+      
+      card.appendChild(imageWrapper);
+      imageWrapper.appendChild(image);
+      card.appendChild(cardBody);
+      //cardBody.appendChild(cardTitle);
+      cardBody.appendChild(clipTitle);
+      cardBody.appendChild(viewCount);
+      cardBody.appendChild(streamer);
+      cardBody.appendChild(creationDate);
+      cardBody.appendChild(duration);
+
+
+      /*
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      */
+
+      
+    }
+
+  });
 }
