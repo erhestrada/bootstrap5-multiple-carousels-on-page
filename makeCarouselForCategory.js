210f10960c278b473ca3e18850b619e6edfc5f89
Erik <180784081+erhetrada@users.noreply.github.com>
2024-10-16 23:41:07 -0400

    add initial carousel html for each carousel in makeCarouselForCategory

--- makeCarouselForCategory.js
+++ makeCarouselForCategory.js
@@ -1,5 +1,28 @@
 // basically a refactor of getTopClips
+import { getTopClips } from "./getTopClips";
 
 export function makeCarouselForCategory(category) {
     console.log(category);
+    //getTopClips(clientId, authToken, 'Grand Theft Auto V', 'Grand Theft Auto V', 1);
+
+    const carouselDiv = `
+        <div id="${category}-carousel" class="carousel slide">
+        <p>Popular Clips</p>
+        <div class="carousel-inner" id="${category}-carousel-inner">
+        </div>
+
+        <button class="carousel-control-prev" type="button">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        </div>
+        `;
+
+    document.body.insertAdjacentHTML('beforeend', carouselDiv);
+    getTopClips(clientId, authToken, category, category, 1);
+
 }
