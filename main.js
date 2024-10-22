63b4381431acf288421f930b9239a38eb4143c7f
Erik <180784081+erhetrada@users.noreply.github.com>
2024-10-22 11:43:41 -0400

    remove replaceCarouselItem and old .then(replaceCarouselItem) since i'm doing it in getTopClips now

--- main.js
+++ main.js
@@ -14,62 +14,8 @@ import { saveClip } from './saveClip.js';
 // 1. youutube, kick
 // 2. reddit label for if posted on reddit, maybe view comments section within site
 // Debug
-// 1. first clip is not correct
-// 2. check if category in category-ids else make request and add to category-ids
-// 3. various errors in console
-
-/*
-var clipIndex = 0;
-
-function replaceCarouselItem(increment) {
-  const currentClip = document.getElementById("current-clip");
-  currentClip.remove();
-
-  const carouselInner = document.querySelector('.carousel-inner');
-  
-  const newItem = document.createElement('div');
-  newItem.className = 'carousel-item iframe-slide active';
-  newItem.id = "current-clip";
-  
-  const flexContainer = document.createElement('div');
-  flexContainer.className = 'd-flex justify-content-center align-items-center';
-  //flexContainer.style.height = '500px';
-  
-  const iframe = document.createElement('iframe');
-
-  // handling the case of going left at the start -- better to just not do anything ? don't display prev-arrow on first clip?
-  clipIndex = clipIndex + increment;
-  if (clipIndex < 0) {
-      clipIndex = 0;
-  }
-
-  iframe.src = localStorage.getItem(clipIndex) + "&parent=localhost&autoplay=true";
-  localStorage.setItem("currentClipUrl", iframe.src);
-
-  iframe.height = '360';
-  iframe.width = '640';
-  iframe.frameBorder = '0';
-  iframe.allowFullscreen = true;
-  
-  flexContainer.appendChild(iframe);
-  newItem.appendChild(flexContainer);
-  carouselInner.appendChild(newItem);
-
-  // Refresh the carousel to recognize the new item
-  const carousel = new bootstrap.Carousel(document.querySelector('#carouselExampleControls'));
-}
-*/
-
-
-/*
-getTopClips(clientId, authToken, "popular-clips", "Just Chatting", 1)
-  .then((clipsData) => {
-    replaceCarouselItem(0);
-  })
-  .catch((error) => {
-    console.error(error);
-});
-*/
+// 1. check if category in category-ids else make request and add to category-ids
+// 2. various errors in console
 
 document.querySelector('.carousel-control-next').addEventListener('click', () => replaceCarouselItem(1));
 document.querySelector('.carousel-control-prev').addEventListener('click', () => replaceCarouselItem(-1));
@@ -93,11 +39,4 @@ document.getElementById('donut-button-right').addEventListener('click', () => up
 document.getElementById('donut-button-bottom').addEventListener('click', () => updateStreamerBarCarouselFromDonut(30));
 document.getElementById('donut-button-left').addEventListener('click', () => updateStreamerBarCarouselFromDonut(365));
 
-
-makeTopCategoriesCarousels()
-  .then(() => {
-    //replaceCarouselItem(0);
-  })
-  .catch((error) => {
-    console.error(error);
-});
+makeTopCategoriesCarousels();
