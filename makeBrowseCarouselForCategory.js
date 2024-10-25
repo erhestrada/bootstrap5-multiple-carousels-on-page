94db6bf180b518e20c587ca2bd11e3bc3eb37e49
Erik <180784081+erhetrada@users.noreply.github.com>
2024-10-25 18:52:47 -0400

    remove excess comments

--- makeBrowseCarouselForCategory.js
+++ makeBrowseCarouselForCategory.js
@@ -1,18 +1,13 @@
-// basically a refactor of getTopClips
 import { getTopClipsBrowse } from "./getTopClipsBrowse";
+import { getTopClips } from "./getTopClips";
 import { makeClipsCarouselSlide } from "./makeClipsCarouselNSlide";
-
-// example box art url
-// 'https://static-cdn.jtvnw.net/ttv-boxart/509658-{width}x{height}.jpg
 
 const boxArtWidth = 200;
 const boxArtHeight = 200;
 
-// thumbnailclicklistener in getTopClips is wrong for this
 export async function makeBrowseCarouselForCategory(category, gameId, boxArtUrl) {
-    const idFormattedCategory = category.replace(/ /g, '-').replace(/:/g, '').replace(/'/g, '');
     const carouselDiv = `
-        <div id="${idFormattedCategory}-carousel" class="carousel slide">
+        <div id="browse-carousel" class="carousel slide">
         <div class="carousel-inner" id="${category}-carousel-inner">
             <img src=${boxArtUrl.replace("{width}", boxArtWidth).replace("{height}", boxArtHeight)} alt="${category}"/>
         </div>
@@ -28,13 +23,13 @@ export async function makeBrowseCarouselForCategory(category, gameId, boxArtUrl)
         </div>
         `;
 
-    const carousel = document.querySelector('.carousel');
+    const carousel = document.getElementById("browse-carousel");
     if (carousel) {
         carousel.remove();
     }
     document.querySelector('nav').insertAdjacentHTML('afterend', carouselDiv);
     await getTopClipsBrowse(clientId, authToken, category, category, 1, undefined, gameId);
+    //await getTopClips(clientId, authToken, category, category, 1, undefined, gameId);
     
     makeClipsCarouselSlide(`${idFormattedCategory}-carousel`);
-
 }
