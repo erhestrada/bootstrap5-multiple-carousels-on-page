81c167d69855012d3d715905379f295bce82eccd
Erik <180784081+erhetrada@users.noreply.github.com>
2024-11-02 20:13:53 -0400

    get clips data for streamer in the last day

--- getClips.js
+++ getClips.js
@@ -1,3 +1,5 @@
+
+
 export async function getClips(clientId, authToken, streamerId = false, gameId = false, daysBack = 1) {
     try {
         const response = await fetch(makeGetUrl(streamerId, gameId, daysBack), {
@@ -17,7 +19,6 @@ export async function getClips(clientId, authToken, streamerId = false, gameId =
     }
 }
 
-
 // The id, game_id, and broadcaster_id query parameters are mutually exclusive.
 function makeGetUrl(streamerId, gameId, daysBack) {
     const currentDateTime = getCurrentDateTime();
@@ -28,9 +29,25 @@ function makeGetUrl(streamerId, gameId, daysBack) {
     } else if (gameId) {
         return "https://api.twitch.tv/helix/clips?game_id=" + gameId + "&started_at=" + pastDateTime + "&ended_at=" + currentDateTime + "&is_featured=false" + "&first=100";  
     }
-
   }
+
+  export function getCurrentDateTime() {
+    const dateTime = new Date();
+    const rfcDateTime = dateTime.toISOString();
+    return rfcDateTime;
+  }
+    
+  export function getPastDateTime(daysBack) {
+    const hoursBack = daysBack * 24;
+    const dateTime = new Date();
+    const pastDateTime = new Date(dateTime.getTime() - hoursBack * 60 * 60 * 1000);
+    const pastRfcDateTime = pastDateTime.toISOString();
+    return pastRfcDateTime;
+  }
+  
 
   function getIdForStreamer(streamer) {
-
+    // curl -X GET 'https://api.twitch.tv/helix/users?login=twitchdev' -H 'Authorization: Bearer ymcfbojkyx4vhe64v0n3sh3jlqbtcj' -H 'Client-Id: dvboj01l2rgahrw7qdafpjags6v98m'
   }
+
+  const clipsDate = await getClips(clientId, authToken, '161737008');
