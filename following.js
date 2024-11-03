544b59ac99fd1a6263239aad0c3ccdcc5e6b1266
Erik <180784081+erhetrada@users.noreply.github.com>
2024-11-02 21:56:27 -0400

    display followedStreamers

--- following.js
+++ following.js
@@ -1,5 +1,12 @@
 function displayFollowedStreamers() {
-    console.log(localStorage.getItem('followedStreamers'));
+    const followedStreamersContainer = document.getElementById('followed-streamers-container');
+    const followedStreamers = JSON.parse(localStorage.getItem('followedStreamers')) || {};
+    
+    for (const streamerName in followedStreamers) {
+        const streamerNameElement = document.createElement('p');
+        streamerNameElement.innerText = streamerName;
+        followedStreamersContainer.appendChild(streamerNameElement);
+    }
 }
 
 displayFollowedStreamers();
