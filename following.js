f15e1517fbb7e2151591166ace62ae26bf0a7943
Erik <180784081+erhetrada@users.noreply.github.com>
2024-11-06 20:55:17 -0500

    add unfollow buttons

--- following.js
+++ following.js
@@ -3,9 +3,19 @@ function displayFollowedStreamers() {
     const followedStreamers = JSON.parse(localStorage.getItem('followedStreamers')) || {};
     
     for (const streamerName in followedStreamers) {
+        const streamerEntry = document.createElement('div');
+        streamerEntry.style.display = 'flex';
+
         const streamerNameElement = document.createElement('p');
         streamerNameElement.innerText = streamerName;
-        followedStreamersContainer.appendChild(streamerNameElement);
+        streamerEntry.appendChild(streamerNameElement);
+
+        const unfollowButton = document.createElement('button');
+        unfollowButton.innerText = 'unfollow';
+        streamerEntry.appendChild(unfollowButton);
+
+        followedStreamersContainer.appendChild(streamerEntry);
+
     }
 }
 
