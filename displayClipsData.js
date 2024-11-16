9a14d6a6de572809ce09a360eb86b6c0d1fd9fad
Erik <180784081+erhetrada@users.noreply.github.com>
2024-11-15 23:48:48 -0500

    move clips from new to old after displaying them in tab

--- displayClipsData.js
+++ displayClipsData.js
@@ -1,12 +1,16 @@
 
-// rename to displayStreamerForYouClipsData
-export function displayClipsData(streamer, clipsDataForStreamer) {
+// clipsData = newClipsInStreamerInbox
+export function displayClipsData(streamer, newClipsInStreamerInbox) {
     let forYouClipsData = JSON.parse(localStorage.getItem('forYouClipsData')) || {};
-    forYouClipsData = updateForYouClipsData(streamer, clipsDataForStreamer, forYouClipsData);
+    //forYouClipsData = updateForYouClipsData(streamer, newClipsInStreamerInbox, forYouClipsData);
     console.log(forYouClipsData);
 
     displayClipsInTab(streamer, forYouClipsData[streamer]['newClipsData'], 'new-clips-container')
     displayClipsInTab(streamer, forYouClipsData[streamer]['oldClipsData'], 'old-clips-container')
+
+    moveClipsFromNewToOld(streamer, forYouClipsData);
+
+    // move new clips to old clips
 }
 
 function displayClipsInTab(streamer, clipsDataForStreamer, containerId) {
@@ -126,45 +130,10 @@ function playClip(embedUrls, index) {
     iframeContainer.appendChild(iframe);
 }
 
-
-function updateForYouClipsData(streamer, clipsDataForStreamer, forYouClipsData) {
-    // Check if the streamer already exists in the data
-    if (!(streamer in forYouClipsData)) {
-        // If not, initialize the data for the streamer
-        console.log('initialize clips data for streamer');
-        forYouClipsData[streamer] = {
-            'newClipsData': clipsDataForStreamer,  // Add all the clips as 'newClipsData'
-            'oldClipsData': []                    // Initialize oldClipsData as an empty array
-        };
-        localStorage.setItem('forYouClipsData', JSON.stringify(forYouClipsData));
-        return forYouClipsData;
-    } else {
-        // If the streamer already exists, loop through the clips
-        console.log(clipsDataForStreamer);
-        for (const clipData of clipsDataForStreamer) {
-            // Check if the clip is already in the newClipsData array
-            const indexToRemove = forYouClipsData[streamer]['newClipsData'].findIndex(item => item.id === clipData.id);  // Find by id
-
-            if (indexToRemove !== -1) {  // If the clip is found in 'newClipsData'
-                console.log('move from new to old', clipData.id);
-                
-                // Remove the clip from 'newClipsData' using splice
-                forYouClipsData[streamer]['newClipsData'].splice(indexToRemove, 1);
-                
-                // Add the clip to 'oldClipsData'
-                forYouClipsData[streamer]['oldClipsData'].push(clipData);
-            } else {         
-                // If the clip wasn't found in 'newClipsData' and it's not in 'oldClipsData', add it as a new clip
-                const oldClipsClipIndex = forYouClipsData[streamer]['oldClipsData'].findIndex(item => item.id === clipData.id);  // Find by id
-                if (oldClipsClipIndex === -1) {
-                    console.log('add new clip', clipData.id);       
-                    forYouClipsData[streamer]['newClipsData'].push(clipData);
-                }
-            }
-        }
-
-        // After updates, save the data to localStorage
-        localStorage.setItem('forYouClipsData', JSON.stringify(forYouClipsData));
-        return forYouClipsData;
-    }
+// streamerInboxes = forYouClipsData
+function moveClipsFromNewToOld(streamer, streamerInboxes) {
+    streamerInboxes[streamer].oldClipsData = streamerInboxes[streamer].oldClipsData.concat(streamerInboxes[streamer].newClipsData)
+    streamerInboxes[streamer].newClipsData = [];
+    localStorage.setItem('forYouClipsData', JSON.stringify(streamerInboxes));
+    return streamerInboxes;
 }
