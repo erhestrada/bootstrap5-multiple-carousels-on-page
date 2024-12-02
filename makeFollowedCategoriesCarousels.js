e2fc66e5c6b593e2f55ca7c32344c12bad4ae80e
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-01 22:43:38 -0500

    empty cateogriesCarousels div when followed-categories button clicked

--- makeFollowedCategoriesCarousels.js
+++ makeFollowedCategoriesCarousels.js
@@ -1,3 +1,17 @@
-export function makeFollowedCategoriesCarousels() {
+import {makeCarouselForCategory} from './makeCarouselForCategory.js'
+
+// i just need to be storing the boxArtUrls and the gameIds when i store the category
+
+export async function makeFollowedCategoriesCarousels() {
     console.log('HELLOOOOOOO');
+    const categoriesCarousels = document.getElementById('categories-carousels');
+    categoriesCarousels.innerHTML = '';
+
+    const followedCategories = localStorage.getItem('followedCategories') || [];
+    const boxArtUrls = clipsData.data.map((pojo) => pojo.box_art_url);
+    const gameIds = clipsData.data.map((pojo) => pojo.id);
+
+    //followedCategories.forEach((category, index) => makeCarouselForCategory(category, gameIds[index], boxArtUrls[index]));
+
+
 }
