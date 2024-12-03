b040ae254b1205ae0654cbec2eda44519625ffbf
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-02 21:21:06 -0500

    implement getBoxArtUrlAndIdForCategory using window.topCategoriesClipsData

--- followCategory.js
+++ followCategory.js
@@ -1,10 +1,9 @@
 export async function followCategory(category) {    
-    const categoryData = await getCategories(category);
-
     let followedCategories = JSON.parse(localStorage.getItem('followedCategories')) || {};
 
     if (!(category in followedCategories)) {
-        followedCategories[category] = {'boxArtUrl': categoryData.data[0].box_art_url, 'categoryId': categoryData.data[0].id};
+        const boxArtUrlAndId = getBoxArtUrlAndIdForCategory(category);
+        followedCategories[category] = {'boxArtUrl': boxArtUrlAndId[0], 'categoryId': boxArtUrlAndId[1]};
     }
     
     localStorage.setItem('followedCategories', JSON.stringify(followedCategories));
@@ -12,22 +11,14 @@ export async function followCategory(category) {
     return followedCategories;
 }
 
-export async function getCategories(query) {
-    try {
-        const url = "https://api.twitch.tv/helix/search/categories?query=" + encodeURIComponent(query);  
-        //const url = "https://api.twitch.tv/helix/search/games?query=" + encodeURIComponent(query);  
-        const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Client-Id': clientId,
-            'Authorization': 'Bearer ' + authToken
+function getBoxArtUrlAndIdForCategory(category) {
+    for (const categoryClipsData of window.topCategoriesClipsData) {
+        if (category === categoryClipsData.name) {
+            const boxArtUrl = categoryClipsData.box_art_url;
+            const id = categoryClipsData.id;
+            return [boxArtUrl, id];
         }
-        });
-
-        const data = await response.json();
-        return data;
-
-    } catch (error) {
-        console.error(error);
     }
+    
+    return ['', ''];
 }
