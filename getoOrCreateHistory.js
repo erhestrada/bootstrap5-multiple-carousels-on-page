5024e89047781e666334c7c2507e4868c88e907c
Erik <180784081+erhetrada@users.noreply.github.com>
2025-04-06 20:58:35 -0400

    implement getOrCreateWatchHistory

--- getoOrCreateHistory.js
+++ getoOrCreateHistory.js
@@ -1,22 +1,8 @@
-export function getOrCreateHistory() {
-    
-}
-
-import { storeUser } from "./storeUser";
-
-export function getOrCreateUniqueId() {
-    let uuid = localStorage.getItem('uniqueId');
-    if (!uuid) {
-      uuid = generateUUID();
-      localStorage.setItem('uniqueId', uuid);
-      storeUser(uuid);
-    }
-    return uuid;
+export function getOrCreateWatchHistory() {
+  let watchHistory = localStorage.getItem('watchHistory');
+  if (!watchHistory) {
+    watchHistory = [];
+    localStorage.setItem('watchHistory', watchHistory);
   }
-
-function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
+  return watchHistory;    
 }
