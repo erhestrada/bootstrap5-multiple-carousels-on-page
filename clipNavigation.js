3326838338747dc7579a3358d552c82208dd4559
Erik <180784081+erhetrada@users.noreply.github.com>
2025-07-01 02:56:10 -0400

    Refactor clip navigation into its own file

---
+++ clipNavigation.js
@@ -0,0 +1,50 @@
+import { replaceCarouselItem } from './getTopClips.js';
+import { updateDonutPfp } from "./updateDonutPfp";
+import { updateStreamerBarCarousel } from "./updateStreamerBarCarousel";
+import { highlightDiv } from './getTopClips.js';
+
+export function playAdjacentClip(arrow) {
+    // in getTopClips.js:
+    // window.clipsData[carouselName] = clipsData;
+    // window.currentClipPosition = {'game': carouselName, 'index': index};
+    let {game, index} = window.currentClipPosition;
+    console.log(JSON.parse(JSON.stringify(window.currentClipPosition)));
+
+    const gameClipsData = clipsData[game].data;
+    const englishGameClipsData = gameClipsData
+        .map((datum, i) => ({ ...datum, originalIndex: i }))
+        .filter(datum => datum.language === 'en');
+    const embedUrls = englishGameClipsData.map(d => d.embed_url);
+    const streamerIds = englishGameClipsData.map(d => d.broadcaster_id);
+    const streamers = englishGameClipsData.map(d => d.broadcaster_name);
+    const originalIndices = englishGameClipsData.map(d => d.originalIndex);
+
+    if (arrow === "next") {
+        window.currentClipPosition.index++;
+        console.log('next clicked');
+
+    } else {
+        if (index > 0) {
+            window.currentClipPosition.index--;
+        }
+    }
+
+    const updatedIndex = window.currentClipPosition.index;
+
+    replaceCarouselItem(updatedIndex, embedUrls, streamerIds, streamers);
+    const originalIndex = originalIndices[updatedIndex];
+    const thumbnailWrapper = window.thumbnailWrappers[`${game}-${originalIndex}`];
+    highlightDiv(thumbnailWrapper);
+
+    // if streamer stays the same, don't have to update streamerBar e.g. clicked into streamerBarCarousel
+    // updateStreamerBar()
+    let carousel2 = document.getElementById('carousel2');
+    const carousel2Inner = document.getElementById('carousel2-inner');
+    carousel2Inner.innerHTML = '';
+  
+    updateDonutPfp(streamerIds[updatedIndex]);
+    updateStreamerBarCarousel(streamerIds[updatedIndex]);
+  
+    carousel2 = new bootstrap.Carousel(document.querySelector('#carousel2'));
+    console.log(JSON.parse(JSON.stringify(window.currentClipPosition)));
+}
--- main.js
+++ main.js
@@ -9,54 +9,7 @@ import { followCategory } from './followCategory.js';
 import { makeFollowedCategoriesCarousels } from './makeFollowedCategoriesCarousels.js';
 import { toggleClipPlayer } from './toggleClipPlayer.js';
 import { highlightDiv } from './getTopClips.js';
-
-
-// need to consider non-english clips
-function playAdjacentClip(arrow) {
-    // in getTopClips.js:
-    // window.clipsData[carouselName] = clipsData;
-    // window.currentClipPosition = {'game': carouselName, 'index': index};
-    let {game, index} = window.currentClipPosition;
-    console.log(JSON.parse(JSON.stringify(window.currentClipPosition)));
-
-    const gameClipsData = clipsData[game].data;
-    const englishGameClipsData = gameClipsData
-        .map((datum, i) => ({ ...datum, originalIndex: i }))
-        .filter(datum => datum.language === 'en');
-    const embedUrls = englishGameClipsData.map(d => d.embed_url);
-    const streamerIds = englishGameClipsData.map(d => d.broadcaster_id);
-    const streamers = englishGameClipsData.map(d => d.broadcaster_name);
-    const originalIndices = englishGameClipsData.map(d => d.originalIndex);
-
-    if (arrow === "next") {
-        window.currentClipPosition.index++;
-        console.log('next clicked');
-
-    } else {
-        if (index > 0) {
-            window.currentClipPosition.index--;
-        }
-    }
-
-    const updatedIndex = window.currentClipPosition.index;
-
-    replaceCarouselItem(updatedIndex, embedUrls, streamerIds, streamers);
-    const originalIndex = originalIndices[updatedIndex];
-    const thumbnailWrapper = window.thumbnailWrappers[`${game}-${originalIndex}`];
-    highlightDiv(thumbnailWrapper);
-
-    // if streamer stays the same, don't have to update streamerBar e.g. clicked into streamerBarCarousel
-    // updateStreamerBar()
-    let carousel2 = document.getElementById('carousel2');
-    const carousel2Inner = document.getElementById('carousel2-inner');
-    carousel2Inner.innerHTML = '';
-  
-    updateDonutPfp(streamerIds[updatedIndex]);
-    updateStreamerBarCarousel(streamerIds[updatedIndex]);
-  
-    carousel2 = new bootstrap.Carousel(document.querySelector('#carousel2'));
-    console.log(JSON.parse(JSON.stringify(window.currentClipPosition)));
-}
+import { playAdjacentClip } from './clipNavigation.js';
 
 window.clipsData = {};
 window.pageNumber = 1;
@@ -67,6 +20,9 @@ localStorage?.removeItem('highlightedDivId');
 
 document.querySelector('#carouselExampleControls .carousel-control-next').addEventListener('click', () => playAdjacentClip('next'));
 document.querySelector('#carouselExampleControls .carousel-control-prev').addEventListener('click', () => playAdjacentClip('prev'));
+document.querySelector('#next-carousel-button').addEventListener('click', () => playAdjacentClip('prev'));
+document.querySelector('#previous-carousel-button').addEventListener('click', () => playAdjacentClip('next'));
+
 
 document.getElementById('like-button').addEventListener('click', () => saveClip("liked-clips"));
 document.getElementById('dislike-button').addEventListener('click', () => saveClip("disliked-clips"));
