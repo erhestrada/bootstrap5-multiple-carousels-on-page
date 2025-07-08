c02cb3eae83e6042a5d0ef396e9ae5ccab2214b2
Erik <180784081+erhetrada@users.noreply.github.com>
2025-07-08 02:06:02 -0400

    Implement slide carousel function to call when thumbnail out of view

--- ensureThumbnailInView.js
+++ ensureThumbnailInView.js
@@ -1,29 +1,3 @@
-export function ensureThumbnailInView(carouselId, highlightedThumbnail) {
-    const carouselInner = document.querySelector(`#${carouselId} .carousel-inner`);
-    if (!carouselInner || !highlightedThumbnail) return;
-
-    const containerRect = carouselInner.getBoundingClientRect();
-    const thumbRect = highlightedThumbnail.getBoundingClientRect();
-
-    const scrollAmount = carouselInner.offsetWidth;
-
-    // Check if the thumbnail is left of visible area
-    if (thumbRect.left < containerRect.left) {
-        carouselInner.scrollBy({
-            left: thumbRect.left - containerRect.left - 10, // extra padding
-            behavior: "smooth"
-        });
-    }
-
-    // Check if the thumbnail is right of visible area
-    else if (thumbRect.right > containerRect.right) {
-        carouselInner.scrollBy({
-            left: thumbRect.right - containerRect.right + 10, // extra padding
-            behavior: "smooth"
-        });
-    }
-}
-
 export function elementIsInView(element, padding = 0) {
     if (!element) return false;
 
@@ -41,4 +15,20 @@ export function elementIsInView(element, padding = 0) {
     }
 
     return 'visible';
+}
+
+export function slideCarousel(carouselId, direction) {
+    const carouselInner = document.querySelector(`#${carouselId} .carousel-inner`);
+
+    let scrollAmount;
+    if (direction === "right") {
+        scrollAmount = carouselInner.offsetWidth;
+    } else {
+        scrollAmount = -carouselInner.offsetWidth;
+    }
+
+    carouselInner.scrollBy({
+        left: scrollAmount,
+        behavior: "smooth"
+    });
 }
