7301c49210261cebf4fd9e1ee9c2ed0df123fa43
Erik <180784081+erhetrada@users.noreply.github.com>
2025-07-19 12:40:32 -0400

    Make new carousels

---
+++ makeTopCategoriesNewCarousels.js
@@ -0,0 +1,27 @@
+import {makeNewCarouselForCategory} from './makeNewCarouselForCategory.js'
+
+// pageNumber x 20
+
+export async function makeTopCategoriesNewCarousels(pageNumber) {
+    try {
+        const url = "https://api.twitch.tv/helix/games/top?first=100";
+        const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Client-Id': clientId,
+            'Authorization': 'Bearer ' + authToken
+        }
+        });
+
+        const clipsData = await response.json();
+        window.topCategoriesClipsData = clipsData.data;
+        const topCategories = clipsData.data.map((pojo) => pojo.name);
+        window.orderedCarousels = topCategories;
+        const boxArtUrls = clipsData.data.map((pojo) => pojo.box_art_url);
+        const gameIds = clipsData.data.map((pojo) => pojo.id);
+        topCategories.forEach((category, index) => makeNewCarouselForCategory(category, gameIds[index], boxArtUrls[index]));
+
+    } catch (error) {
+        console.error(error);
+    }
+}
