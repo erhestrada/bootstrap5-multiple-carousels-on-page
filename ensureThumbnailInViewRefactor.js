b0cdaa13ad041d6dc1a63c9c3a5a1365cee84769
Erik <180784081+erhetrada@users.noreply.github.com>
2025-07-23 17:39:46 -0400

    Make separate file for carousel refactored javascript

---
+++ ensureThumbnailInViewRefactor.js
@@ -0,0 +1,93 @@
+export let carouselIsSliding = false;
+
+export function elementInViewHorizontally(element, padding = 0) {
+    if (!element) return false;
+
+    const rect = element.getBoundingClientRect();
+
+    // viewport's left edge = 0
+    // viewport's right edge = window.innerWidth
+
+    if (rect.left < 0) {
+        return 'left';
+    }
+
+    if (rect.right > window.innerWidth - padding) {
+        return 'right';
+    }
+
+    return 'visible';
+}
+
+export function slideCarousel(carouselId, direction) {
+    carouselIsSliding = true;
+
+    // Get carousel within carousel row
+    const carousel = document.getElementById(`${carouselId}-row`).querySelector('.carousel');
+
+    let scrollAmount;
+    if (direction === "right") {
+        scrollAmount = carousel.offsetWidth;
+    } else if (direction === "left") {
+        scrollAmount = -carousel.offsetWidth;
+    } else if (direction === "reset") {
+        carousel.scrollLeft = 0;
+    }
+
+    carousel.scrollBy({
+        left: scrollAmount,
+        behavior: "smooth"
+    });
+
+    setTimeout(() => {
+        carouselIsSliding = false;
+    }, 400);
+}
+
+// if clipPlayer open ...
+// if clipPlayer closed ...
+export function elementInViewVertically(element, padding = 0) {
+    if (!element) return false;
+
+    const elementRect = element.getBoundingClientRect();
+    const stickyHeight = getHeightOfElement('.sticky-stuff');
+
+    if (elementRect.top < stickyHeight) {
+        return 'above';
+    }
+
+    if (elementRect.bottom > window.innerHeight - padding) {
+        return 'below';
+    }
+
+    return 'visible';
+}
+
+// if highlighted thumbnail below viewport
+export function scrollDownToThumbnail() {
+    const carouselHeight = getHeightOfElement('.carousel-inner.thumbnails-carousel-inner');
+    console.log('carousel height: ', carouselHeight);
+
+    window.scrollBy({
+        top: carouselHeight,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+// have to scroll up by more because of the header
+export function scrollUpToThumbnail() {
+    const carouselHeight = getHeightOfElement('.carousel-inner.thumbnails-carousel-inner');
+    console.log('carousel height: ', carouselHeight);
+
+    window.scrollBy({
+        top: -carouselHeight, // negative to scroll up
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+function getHeightOfElement(selector) {
+    const element = document.querySelector(selector);
+    return element ? element.offsetHeight : 0;
+}
