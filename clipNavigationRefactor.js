9b44fd61a6680e1cb608e615498662b120e098da
Erik <180784081+erhetrada@users.noreply.github.com>
2025-07-23 18:43:58 -0400

    Import highlightDiv from refactored file

--- clipNavigationRefactor.js
+++ clipNavigationRefactor.js
@@ -1,7 +1,7 @@
 import { replaceCarouselItem } from './getTopClips.js';
 import { updateDonutPfp } from "./updateDonutPfp";
 import { updateStreamerBarCarousel } from "./updateStreamerBarCarouselRefactor";
-import { highlightDiv } from './getTopClips.js';
+import { highlightDiv } from './getTopClipsRefactor.js';
 import { elementInViewHorizontally, elementInViewVertically, scrollDownToThumbnail, scrollUpToThumbnail, slideCarousel } from './ensureThumbnailInViewRefactor.js';
 import { makeCarouselId } from './makeCarouselForCategory.js';
 import { carouselIsSliding } from './ensureThumbnailInViewRefactor.js';
