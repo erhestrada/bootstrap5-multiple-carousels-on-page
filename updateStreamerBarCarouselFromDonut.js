3b175e13096b3fbef9691e6b0cee3074c1489d2b
Erik <180784081+erhetrada@users.noreply.github.com>
2025-08-18 00:30:18 -0400

    Remove updateStreamerBarCarouselFromDonut.js

--- updateStreamerBarCarouselFromDonut.js
+++
@@ -1,5 +0,0 @@
-import { updateStreamerBarCarousel } from './updateStreamerBarCarousel.js'
-
-export function updateStreamerBarCarouselFromDonut(daysBack) {  
-    updateStreamerBarCarousel(window.currentStreamerId, daysBack);
-}
