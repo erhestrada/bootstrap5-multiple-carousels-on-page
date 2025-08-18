0eabe65e73e651958a7916c30ddc571d979ebd4c
Erik <180784081+erhetrada@users.noreply.github.com>
2025-08-18 00:21:44 -0400

    Use window current streamer id

--- updateStreamerBarCarouselFromDonut.js
+++ updateStreamerBarCarouselFromDonut.js
@@ -1,6 +1,5 @@
 import { updateStreamerBarCarousel } from './updateStreamerBarCarousel.js'
 
 export function updateStreamerBarCarouselFromDonut(daysBack) {  
-    const currentStreamerId = localStorage.getItem('currentClipStreamerId');
-    updateStreamerBarCarousel(currentStreamerId, daysBack);
+    updateStreamerBarCarousel(window.currentStreamerId, daysBack);
 }
