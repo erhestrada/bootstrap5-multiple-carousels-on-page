9db173696fa768c0ee62f4198dfe09b1d8737b00
Erik <180784081+erhetrada@users.noreply.github.com>
2025-08-18 00:01:17 -0400

    Simplify updateStreamerBarCarouselFromDonut removing old carousel2 stuff

--- updateStreamerBarCarouselFromDonut.js
+++ updateStreamerBarCarouselFromDonut.js
@@ -1,13 +1,6 @@
 import { updateStreamerBarCarousel } from './updateStreamerBarCarousel.js'
 
-export function updateStreamerBarCarouselFromDonut(daysBack) {
-    let carousel2 = document.getElementById('carousel2');
-    const carousel2Inner = document.getElementById('carousel2-inner');
-    carousel2Inner.innerHTML = '';
-  
+export function updateStreamerBarCarouselFromDonut(daysBack) {  
     const currentStreamerId = localStorage.getItem('currentClipStreamerId');
     updateStreamerBarCarousel(currentStreamerId, daysBack);
-  
-    carousel2 = new bootstrap.Carousel(document.querySelector('#carousel2'));
-
 }
