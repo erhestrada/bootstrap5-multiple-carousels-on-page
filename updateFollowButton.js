d89f0e5a962ee4ee003067609284e0c7c0c3f27f
Erik <180784081+erhetrada@users.noreply.github.com>
2025-08-19 06:21:20 -0400

    Call updateFollowButton when replacing carousel item

--- updateFollowButton.js
+++ updateFollowButton.js
@@ -1,14 +1,14 @@
 export function updateFollowButton(streamer, category) {
     const followButton = document.getElementById('compound-follow-button');
-    const followedStreamers = JSON.parse(localStorage.getItem('followedStreamers'));
-    const followedCategories = JSON.parse(localStorage.getItem('folloedCategories'));
+    const followedStreamers = JSON.parse(localStorage.getItem('followedStreamers') || '{}');
+    const followedCategories = JSON.parse(localStorage.getItem('followedCategories') || '{}');
 
     displayHeart(streamer, followedStreamers);
     displayHeart(category, followedCategories);
 }
 
-function displayHeart(value, list) {
-    if (followedStreamers.includes(streamer)) {
+function displayHeart(value, object) {
+    if (value in object) {
 
     } else {
 
