94a1598da9af678d28e2733bb785e96fa9b7598a
Erik <180784081+erhetrada@users.noreply.github.com>
2025-08-19 06:06:14 -0400

    Get followed streamers and followed categories, initialize function to display the appropriate heart

--- updateFollowButton.js
+++ updateFollowButton.js
@@ -1,3 +1,16 @@
-function updateFollowButton() {
-    
+function updateFollowButton(streamer, category) {
+    const followButton = document.getElementById('compound-follow-button');
+    const followedStreamers = JSON.parse(localStorage.getItem('followedStreamers'));
+    const followedCategories = JSON.parse(localStorage.getItem('folloedCategories'));
+
+    displayHeart(streamer, followedStreamers);
+    displayHeart(category, followedCategories);
+}
+
+function displayHeart(value, list) {
+    if (followedStreamers.includes(streamer)) {
+
+    } else {
+
+    }
 }
