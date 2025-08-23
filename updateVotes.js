db672bec5b029444ffc4a1bc4a0e8b367072aa32
Erik <180784081+erhetrada@users.noreply.github.com>
2025-08-23 02:27:23 -0400

    Simplify getting opposite vote icon

--- updateVotes.js
+++ updateVotes.js
@@ -1,35 +1,30 @@
 export function updateVotes(button, vote) {
-  const totalVotesElement = document.getElementById('total-votes');
-  let totalVotes = parseInt(totalVotesElement.textContent, 10) || 0;
+    const totalVotesElement = document.getElementById('total-votes');
+    let totalVotes = parseInt(totalVotesElement.textContent, 10) || 0;
 
-  // If vote is upvote other vote is downvote and vice versa
-  const oppositeVote = vote === 'upvote' ? 'downvote' : 'upvote';
-  const voteButtons = {
-    upvote: document.getElementById('upvote-button'),
-    downvote: document.getElementById('downvote-button')
-  };
+    const voteIcon = button.querySelector('.vote-icon');
 
-  const voteIcon = button.querySelector('.vote-icon');
-  const oppositeVoteIcon = voteButtons[oppositeVote].querySelector('.vote-icon');
+    const oppositeButton = document.getElementById(vote === 'upvote' ? 'downvote-button' : 'upvote-button');
+    const oppositeVoteIcon = oppositeButton.querySelector('.vote-icon');
 
-  const voteIconAlreadyClicked = voteIcon.classList.contains('voted');
-  const oppositeVoteIconAlreadyClicked = oppositeVoteIcon.classList.contains('voted');
+    const voteIconAlreadyClicked = voteIcon.classList.contains('voted');
+    const oppositeVoteIconAlreadyClicked = oppositeVoteIcon.classList.contains('voted');
 
-  if (voteIconAlreadyClicked) {
+    if (voteIconAlreadyClicked) {
     voteIcon.classList.remove('voted');
     // turning upvote off -> -1, turning downvote off -> +1
     totalVotes += vote === 'upvote' ? -1 : 1;
-  } else {
+    } else {
     voteIcon.classList.add('voted');
     // +1 for upvote -1 for downvote
     totalVotes += vote === 'upvote' ? 1 : -1;
 
     if (oppositeVoteIconAlreadyClicked) {
-      oppositeVoteIcon.classList.remove('voted');
-      // if vote is 'upvote' opposite vote is downvote and removing downvote -> +1; if vote is 'downvote' opposite vote is upvote and removing upvote -> -1
-      totalVotes += vote === 'upvote' ? 1 : -1;
+        oppositeVoteIcon.classList.remove('voted');
+        // if vote is 'upvote' opposite vote is downvote and removing downvote -> +1; if vote is 'downvote' opposite vote is upvote and removing upvote -> -1
+        totalVotes += vote === 'upvote' ? 1 : -1;
     }
-  }
+    }
 
-  totalVotesElement.textContent = totalVotes;
+    totalVotesElement.textContent = totalVotes;
 }
