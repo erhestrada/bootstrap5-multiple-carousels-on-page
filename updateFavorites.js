90a605ec9dbe1add2ad84924cf2a44d3a6744c6f
Erik <180784081+erhetrada@users.noreply.github.com>
2025-08-24 13:32:56 -0400

    Fill and unfill the favorite star when clicked

--- updateFavorites.js
+++ updateFavorites.js
@@ -6,10 +6,14 @@ export function updateFavorites(button) {
   const favoritesStorageKey = 'favoritedClips';
 
   if (isFavorited) {
-    favoriteIcon.classList.remove('favorited');
+    favoriteIcon.classList.remove('favorited'); // Remove fill color
+    favoriteIcon.classList.remove('bi-star-fill');
+    favoriteIcon.classList.add('bi-star');
     removeClip(favoritesStorageKey);
   } else {
-    favoriteIcon.classList.add('favorited');
+    favoriteIcon.classList.add('favorited'); // Add fill color
+    favoriteIcon.classList.remove('bi-star');
+    favoriteIcon.classList.add('bi-star-fill');
     saveClip(favoritesStorageKey);
   }
 }
