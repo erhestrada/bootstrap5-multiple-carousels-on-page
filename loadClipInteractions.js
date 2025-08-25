d6119bae49b7172eb2ee42ce0b637a899c19c8f4
Erik <180784081+erhetrada@users.noreply.github.com>
2025-08-25 14:05:27 -0400

    Load user interactions

--- loadClipInteractions.js
+++ loadClipInteractions.js
@@ -1,3 +1,8 @@
 export function loadClipInteractions() {
-    
+    const upvotedClips = JSON.parse(localStorage.getItem('upvotedClips') || '[]');
+    const downvotedClips = JSON.parse(localStorage.getItem('downvotedClips') || '[]');
+    const favoritedClips = JSON.parse(localStorage.getItem('favoritedClips') || '[]');
+
+    console.log(upvotedClips, downvotedClips, favoritedClips);
+
 }
