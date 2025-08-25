085973b7790f20bae2679d1abed4314839f2b689
Erik <180784081+erhetrada@users.noreply.github.com>
2025-08-25 15:31:01 -0400

    Add voted class when user has already voted on clip

--- loadClipInteractions.js
+++ loadClipInteractions.js
@@ -3,6 +3,22 @@ export function loadClipInteractions(clip) {
     const downvotedClips = JSON.parse(localStorage.getItem('downvotedClips') || '[]');
     const favoritedClips = JSON.parse(localStorage.getItem('favoritedClips') || '[]');
 
-    console.log(upvotedClips, downvotedClips, favoritedClips);
+    const upvoteButton = document.getElementById('upvote-button');
+    const downvoteButton = document.getElementById('downvote-button');
+    const favoriteButton = document.getElementById('favorite-button');
 
+    console.log(upvotedClips, downvotedClips, favoritedClips);
+    console.log(clip);
+    console.log(upvotedClips.some(upvotedClip => upvotedClip.id === clip.id));
+
+    x(clip, upvotedClips, upvoteButton);
+    x(clip, downvotedClips, downvoteButton);
+    x(clip, favoritedClips, favoriteButton);
+}
+
+function x(clip, memory, element) {
+    if (memory.some(element => element.id === clip.id)) {
+        console.log('heyo');
+        element.classList.add('voted');
+    }
 }
