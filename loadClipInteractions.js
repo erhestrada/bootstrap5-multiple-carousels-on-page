0ebb9e56624acc271cd4648968005f514a8fe87b
Erik <180784081+erhetrada@users.noreply.github.com>
2025-08-25 16:07:22 -0400

    Remove console.logs, rename element to button

--- loadClipInteractions.js
+++ loadClipInteractions.js
@@ -9,21 +9,17 @@ export function loadClipInteractions(clip) {
     const downvoteButton = document.getElementById('downvote-button');
     const favoriteButton = document.getElementById('favorite-button');
 
-    console.log(upvotedClips, downvotedClips, favoritedClips);
-    console.log(clip);
-    console.log(upvotedClips.some(upvotedClip => upvotedClip.id === clip.id));
-
     highlightElement(clip, upvotedClips, upvoteButton, 'vote');
     highlightElement(clip, downvotedClips, downvoteButton, 'vote');
     highlightElement(clip, favoritedClips, favoriteButton, 'favorite');
 }
 
-function highlightElement(clip, memory, element, category) {
-    if (memory.some(element => element.id === clip.id)) {
+function highlightElement(clip, memory, button, category) {
+    if (memory.some(button => button.id === clip.id)) {
         if (category === 'vote') {
-            element.classList.add('voted');
+            button.classList.add('voted');
         } else if (category === 'favorite') {
-            element.classList.add('voted');
+            button.classList.add('voted');
         }
     }
 }
