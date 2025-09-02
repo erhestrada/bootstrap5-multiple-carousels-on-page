75b9877fef8b0365048f2f5036b1a2d222dd883f
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-02 19:00:24 -0400

    Implement function to make request for user activity

--- loadVotes.js
+++ loadVotes.js
@@ -1,8 +1,24 @@
+const uuid = 1;
+
 export function loadVotes() {
 
 }
 
-// upvotes downvotes favorites
-export function loadUserActions() {
+// upvotes downvotes favorites comments
+export async function loadUserActivity() {
+    try {
+        const response = await fetch(`https://192.168.86.195:3000/${uuid}/activity`);
 
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const responseData = await response.json();
+        return responseData;
+        
+    } catch(error) {
+        console.error("Error loading user activity", error);
+        throw new Error(`loadUserActivity failed: ${error.message}`);
+    }
 }
+
