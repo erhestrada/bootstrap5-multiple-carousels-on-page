bc7d43abed2f3a293b1f5bbc97855c98f1af270d
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-02 23:24:46 -0400

    Rename result to postedComment

--- loadVotes.js
+++ loadVotes.js
@@ -38,29 +38,12 @@ export async function postComment(userId, comment) {
             throw new Error(`HTTP error; status: ${response.status}`);
         }
 
-        const result = await response.json();
-        return result;
+        const postedComment = await response.json();
+        return postedComment;
 
     } catch (error) {
         console.error("Error posting comment to database", error);
         throw new Error(`Error posting comment to database: ${error.message}`);
     }
-
 }
 
-
-
-export async function updateBondRequest(receiverId, bondedIntentionsJson, updatedStatus) {
-    try {
-        const response = await fetch('http://192.168.86.195:3000/updateBondRequest', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ receiverId, bondedIntentionsJson, updatedStatus })
-        });
-          const result = await response.json();
-          console.log('Data Stored:', result);
-    
-      } catch (error) {
-          console.error('Error storing data:', error);
-      }
-}
