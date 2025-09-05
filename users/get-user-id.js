c0cc5f31c92120a414358778b58e776dfd50662d
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-05 15:33:43 -0400

    Implement function to get userId given clientId

--- users/get-user-id.js
+++ users/get-user-id.js
@@ -1,3 +1,16 @@
 export default async function getUserId(clientId) {
+    try {
+        const response = await fetch(`http://192.168.86.195:3000/clients/${clientId}`);
 
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const userId = response.json();
+        return userId;
+
+    } catch (error) {
+        console.error('Error loading userId', error);
+        return null;
+    }
 }
