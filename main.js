3527d84c9d4fbe3e87c70682ecf9a848718c6038
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-05 15:10:55 -0400

    Import and call getUserId

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ import { updateStreamerBarCarousel } from './updateStreamerBarCarousel.js'
 import { updateVotes } from './updateVotes.js'
 import { updateFavorites } from './updateFavorites.js';
 import { getClientId } from './get-client-id.js';
+import { getUserId } from './users/index.js';
 
 window.clientId = getClientId();
+window.userId = getUserId(window.clientId);
 window.clipsData = {};
 window.firstThumbnail = false;
 window.thumbnailWrappers = {}; // The divs that are highlighted
