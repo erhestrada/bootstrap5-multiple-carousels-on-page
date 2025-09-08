a7ca7f37ab0f9fd76272ed35785ff753d62944f3
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-08 01:22:07 -0400

    Implement function to make delete vote request

---
+++ votes/delete-vote.js
@@ -0,0 +1,19 @@
+export default async function deleteVote(userId, clipId) {
+    try {
+        const response = await fetch('http://192.168.86.195:3000/votes', {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ userId, clientId, clipId, vote })
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const voteId = await response.json();
+        return voteId;
+    
+    } catch(error) {
+        console.error("Error deleting vote from database", error);
+    }
+}
