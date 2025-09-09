4b8aed96bc1534cbd65c083f70d01a4112e25fa6
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-09 04:14:39 -0400

    Post and delete favorite from backend

--- updateFavorites.js
+++ updateFavorites.js
@@ -1,4 +1,6 @@
 import { saveClip, removeClip } from "./updateVotes.js";
+import { postFavorite, deleteFavorite } from "./favorites/index.js";
+import { getClipId } from "./getClipId.js";
 
 export function updateFavorites(button) {
   const favoriteIcon = button.querySelector('.favorite-icon');
@@ -9,9 +11,14 @@ export function updateFavorites(button) {
   favoriteIcon.classList.toggle('bi-star-fill', isFavorited); // filled if favorited
   favoriteIcon.classList.toggle('bi-star', !isFavorited); // empty if not favorited
 
+  const userId = window.userId;
+  const clipId = getClipId();
+
   if (isFavorited) {
     saveClip(favoritesStorageKey);
+    postFavorite(userId, clipId);
   } else {
     removeClip(favoritesStorageKey);
+    deleteFavorite(userId, clipId);
   }
 }
