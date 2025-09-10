769b5a0444c5f378f0c0f10f33cd66bd34d23a7d
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-10 05:04:00 -0400

    Export getNetVotes and getUserVoteOnClip

--- votes/index.js
+++ votes/index.js
@@ -1,2 +1,4 @@
-export { default as postVote } from './post-vote';
-export { default as deleteVote } from './delete-vote';
+export { default as postVote } from './post-vote.js';
+export { default as deleteVote } from './delete-vote.js';
+export { default as getNetVotes } from './get-net-votes.js';
+export { default as getUserVoteOnClip } from './get-user-vote-on-clip.js';
