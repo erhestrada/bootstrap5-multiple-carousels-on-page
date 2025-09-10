94fc218e438d59533118cafc8b11842ac5e5e22a
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-10 05:04:50 -0400

    Add default

--- votes/get-user-vote-on-clip.js
+++ votes/get-user-vote-on-clip.js
@@ -1,4 +1,4 @@
-export async function getUserVoteOnClip(userId, clipId) {
+export default async function getUserVoteOnClip(userId, clipId) {
     try {
         const response = await fetch(`http://192.168.86.195:3000/votes/${userId}/${clipId}`);
 
