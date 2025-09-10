3fd2a0186e8883a169047d880661daec6de9d6a0
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-09 23:58:26 -0400

    Remove excess space

--- display-net-votes.js
+++ display-net-votes.js
@@ -2,7 +2,6 @@ import { getNetVotes } from './votes/get-net-votes.js'
 
 export async function displayNetVotes(clipId) {
     const netVotes = await getNetVotes(clipId);
-
     const netVotesElement = document.getElementById('net-votes');
     netVotesElement.textContent = netVotes;
 }
