a8a162d0d45b492b032ad67f0deb2cebf7ecf9e1
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-11 14:06:44 -0400

    Implement function to make get request on whether the clip has been favorited or not

---
+++ favorites/get-user-favorite-on-clip.js
@@ -0,0 +1,17 @@
+export default async function getUserFavoriteOnClip(userId, clipId) {
+    try {
+        const response = await fetch(`http://192.168.86.195:3000/favorites/${userId}/${clipId}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const favoritedData = await response.json();
+        const favorited = favoritedData.favorited ?? false; // If user hasn't favorited the clip favorited is false
+        return favorited;
+
+    } catch (error) {
+        console.error('Error getting user favorite on clip', error);
+        return null;
+    }
+}
