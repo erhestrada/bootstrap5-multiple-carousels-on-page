5a68a3f8980e1794a9421c2c128a07c72fb4c16d
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-11 14:24:36 -0400

    Export getFavoriteStatusOfClip

--- favorites/index.js
+++ favorites/index.js
@@ -1,2 +1,3 @@
 export { default as postFavorite } from './post-favorite.js';
 export { default as deleteFavorite } from './delete-favorite.js';
+export { default as getFavoriteStatusOfClip } from './get-favorite-status-of-clip.js';
