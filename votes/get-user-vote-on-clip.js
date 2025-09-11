2605a41e095e9f20eea32eecd6aeb816216b2e27
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-11 13:06:36 -0400

    Rename vote variables, use ?? instead of || to only trigger on undefined or null, use null instead of 0 as fallback value

--- votes/get-user-vote-on-clip.js
+++ votes/get-user-vote-on-clip.js
@@ -6,9 +6,9 @@ export default async function getUserVoteOnClip(userId, clipId) {
             throw new Error(`HTTP error; status: ${response.status}`);
         }
 
-        const votes = await response.json();
-        const netVotes = votes.netVotes || 0; // If no votes on a clip its default score is 0
-        return netVotes;
+        const voteData = await response.json();
+        const vote = voteData.vote ?? null; // If user hasn't voted on the clip vote is null
+        return vote;
 
     } catch (error) {
         console.error('Error getting user vote on clip', error);
