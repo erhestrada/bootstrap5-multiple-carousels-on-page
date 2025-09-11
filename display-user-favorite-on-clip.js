6b47ae63c7debe6cd14238d9def68ff42bbd017d
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-11 14:03:09 -0400

    Implement function to highlight the favorite star on a clip if user has favorited the clip

---
+++ display-user-favorite-on-clip.js
@@ -0,0 +1,19 @@
+import { getUserFavoriteOnClip } from './votes'
+
+export async function displayUserFavoriteOnClip(userId, clipId) {
+  try {
+    // true or false
+    const favorited = await getUserFavoriteOnClip(userId, clipId);
+
+    const favoriteButton = document.getElementById('favorite-button');
+    const favoriteIcon = favoriteButton.querySelector('.favorite-icon');
+
+    favoriteIcon.classList.remove('favorited');
+
+    if (favorited) {
+      favoriteIcon.classList.add('favorited');
+    } 
+  } catch (error) {
+    console.error('Failed to display vote:', error);
+  }
+}
