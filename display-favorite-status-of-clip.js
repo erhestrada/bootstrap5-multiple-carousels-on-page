27bace6cb99f66fe9486bc8d2fe5baa3c029851a
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-11 15:04:45 -0400

    Reset favoriteIcon to empty star and replace with filled star if favorited

--- display-favorite-status-of-clip.js
+++ display-favorite-status-of-clip.js
@@ -2,17 +2,20 @@ import { getFavoriteStatusOfClip } from './favorites'
 
 export async function displayFavoriteStatusOfClip(userId, clipId) {
   try {
-    // true or false
     const favorited = await getFavoriteStatusOfClip(userId, clipId);
 
     const favoriteButton = document.getElementById('favorite-button');
     const favoriteIcon = favoriteButton.querySelector('.favorite-icon');
 
+    // Reset favoriteIcon to empty star
     favoriteIcon.classList.remove('favorited');
+    favoriteIcon.classList.remove('bi-star-fill');
+    favoriteIcon.classList.add('bi-star');
 
     if (favorited) {
       favoriteIcon.classList.add('favorited');
-    } 
+      favoriteIcon.classList.replace('bi-star', 'bi-star-fill');
+    }
   } catch (error) {
     console.error('Failed to display vote:', error);
   }
