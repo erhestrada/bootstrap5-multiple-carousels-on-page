800601935c96db5b553f1fbb92e2d5e61ceb59d3
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-13 05:58:16 -0400

    Implement helper function to make api requests

---
+++ make-api-request.js
@@ -0,0 +1,14 @@
+export async function makeApiRequest(url, options) {
+  try {
+    const response = await fetch(url, options)
+
+    if (!response.ok) {
+      throw new Error(`HTTP error; status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.error('API Request failed:', err);
+    throw err;
+  }
+}
