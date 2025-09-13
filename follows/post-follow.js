5577a0f331cf784f1acdf07bf0ed1b71ed249bb6
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-12 22:45:55 -0400

    Implement function to post follow

---
+++ follows/post-follow.js
@@ -0,0 +1,27 @@
+// label e.g. streamer, category
+export default async function postFollow(userId, name, label) {
+    try {
+        if (!['streamer', 'category'].includes(label)) {
+            throw new Error('Invalid label');
+        }
+
+        const url = `http://192.168.86.195:3000/users/${userId}/following/${label}/${name}`;
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({}),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const followingData = await response.json();
+        const following = followingData.following ?? false;
+        return following;
+
+    } catch (error) {
+        console.error('Error getting follow status:', error);
+        return false;
+    }
+}
