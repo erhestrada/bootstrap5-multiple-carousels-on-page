24ffc7ede88ae2161fba877047a5465f5f412ec6
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-12 22:09:32 -0400

    Refactor path to distinguish between streamer and category requests

--- follows/get-follow-status.js
+++ follows/get-follow-status.js
@@ -1,17 +1,23 @@
-export default async function getFollowStatus(userId, streamerOrCategory) {
+// label e.g. streamer, category
+export default async function getFollowStatus(userId, name, label) {
     try {
-        const response = await fetch(`http://192.168.86.195:3000/users/${userId}/following/${streamerOrCategory}`);
+        if (!['streamer', 'category'].includes(label)) {
+            throw new Error('Invalid label');
+        }
+
+        const url = `http://192.168.86.195:3000/users/${userId}/following/${label}/${name}`;
+        const response = await fetch(url);
 
         if (!response.ok) {
             throw new Error(`HTTP error; status: ${response.status}`);
         }
 
         const followingData = await response.json();
-        const following = followingData.following ?? false; // If user hasn't followed return false
+        const following = followingData.following ?? false;
         return following;
 
     } catch (error) {
-        console.error('Error getting user favorite on clip', error);
-        return false; // Assume default value of false if error getting following status
+        console.error('Error getting follow status:', error);
+        return false;
     }
 }
