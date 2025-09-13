aa6f81eba9a00ff548e7e30274b44b8c16cfb64c
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-13 07:36:56 -0400

    Implement barrel file to export comment api functions

---
+++ comments/index.js
@@ -0,0 +1,4 @@
+export { default as getComments } from './get-comments';
+export { default as postComment } from './post-comment';
+export { default as deleteComment } from './delete-comment';
+export { default as editComment } from './edit-comment';
