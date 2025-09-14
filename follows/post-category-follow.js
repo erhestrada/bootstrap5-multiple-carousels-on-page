4243a4099da939b1a0f9c3b48342bf7f0aa57883
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-14 04:41:35 -0400

    Implement function to post a category to follow

---
+++ follows/post-category-follow.js
@@ -0,0 +1,24 @@
+// modifying this first to work for just categoryFollow
+export default async function postCategoryFollow(userId, category, twitchId) {
+    try {
+        // app.post('/users/:userId/following/categorys/:category/:twitchId', (req, res) => {
+        const url = `http://192.168.86.195:3000/users/${userId}/following/categories/${category}/${twitchId}`;
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({}),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const followingData = await response.json();
+        const following = followingData.following ?? false;
+        return following;
+
+    } catch (error) {
+        console.error('Error following category:', error);
+        return false;
+    }
+}
