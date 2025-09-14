0e3660b921f4bb084dc7e9ec20ae6af38f024ed0
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-14 04:42:25 -0400

    Implement function to delete a followed category

---
+++ follows/delete-category-follow.js
@@ -0,0 +1,24 @@
+// modifying this first to work for just categoryFollow
+export default async function deleteCategoryFollow(userId, category, twitchId) {
+    try {
+        // app.post('/users/:userId/following/categorys/:category/:twitchId', (req, res) => {
+        const url = `http://192.168.86.195:3000/users/${userId}/following/categories/${category}/${twitchId}`;
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({}),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const followingData = await response.json();
+        const following = followingData.following ?? false;
+        return following;
+
+    } catch (error) {
+        console.error('Error unfollowing category:', error);
+        return false;
+    }
+}
