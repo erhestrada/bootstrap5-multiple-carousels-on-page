15cbada293162feae2f842ed23740fb8c94d70bd
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-14 04:44:47 -0400

    Export postCategoryFollow and deleteCategoryFollow

--- follows/index.js
+++ follows/index.js
@@ -1,3 +1,5 @@
 export { default as getFollowStatus } from './get-follow-status';
 export { default as postStreamerFollow } from './post-streamer-follow';
 export { default as deleteStreamerFollow } from './delete-streamer-follow';
+export { default as postCategoryFollow } from './post-category-follow';
+export { default as deleteCategoryFollow } from './delete-category-follow';
