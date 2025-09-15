a637f7b60a653b008cf0160ae1d133bd963e536e
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-15 12:12:42 -0400

    Get followedStreamers and followedCategories from follows

--- display-follow-status.js
+++ display-follow-status.js
@@ -1,6 +1,9 @@
 export async function displayFollowStatus(streamer, category, follows) {
-    displayHeart('follow-streamer-button', streamer, follows.streamers);
-    displayHeart('follow-category-button', category, follows.categories);
+    const followedStreamers = follows.streamers.map(({ streamer }) => streamer);
+    const followedCategories = follows.categories.map(({ category }) => category);
+
+    displayHeart('follow-streamer-button', streamer, followedStreamers);
+    displayHeart('follow-category-button', category, followedCategories);
 }
 
 export function displayHeart(buttonId, name, follows) {
