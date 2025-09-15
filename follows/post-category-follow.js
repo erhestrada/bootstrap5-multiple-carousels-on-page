628ab1f17803ada4c26eb3eda03d888746cad2d1
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-15 06:48:08 -0400

    Send category twitchId and boxArtUrl in the request body

--- follows/post-category-follow.js
+++ follows/post-category-follow.js
@@ -1,12 +1,12 @@
 // modifying this first to work for just categoryFollow
-export default async function postCategoryFollow(userId, category, twitchId) {
+export default async function postCategoryFollow(userId, category, twitchId, boxArtUrl) {
     try {
         // app.post('/users/:userId/following/categorys/:category/:twitchId', (req, res) => {
-        const url = `http://192.168.86.195:3000/users/${userId}/following/categories/${category}/${twitchId}`;
+        const url = `http://192.168.86.195:3000/users/${userId}/following/categories/${category}`;
         const response = await fetch(url, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify({}),
+            body: JSON.stringify({ category, twitchId, boxArtUrl }),
         });
 
         if (!response.ok) {
