3f03506f0e5bacacc7b93dcdfa47c88806d39f6f
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-15 05:48:24 -0400

    Export getKindOfFollows

--- follows/index.js
+++ follows/index.js
@@ -1,4 +1,5 @@
 export { default as getFollows } from './get-follows';
+export { default as getKindOfFollows } from './get-kind-of-follows';
 export { default as postStreamerFollow } from './post-streamer-follow';
 export { default as deleteStreamerFollow } from './delete-streamer-follow';
 export { default as postCategoryFollow } from './post-category-follow';
