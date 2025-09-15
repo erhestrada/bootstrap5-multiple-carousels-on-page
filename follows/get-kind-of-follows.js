01886738c154f3b03233bc2ef43a77cd4a22b055
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-15 05:47:49 -0400

    Implement function to get follows of a specific kind e.g. followed streamers or categories

---
+++ follows/get-kind-of-follows.js
@@ -0,0 +1,23 @@
+// label e.g. streamer, category
+export default async function getKindOfFollows(userId, kind) {
+    try {
+        if (!['streamers', 'categories'].includes(kind)) {
+            throw new Error('Kind must be streamers or categories');
+        }
+
+        const url = `http://192.168.86.195:3000/users/${userId}/following/${kind}`;
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const followingData = await response.json();
+        const follows = followingData[kind];
+        return follows;
+
+    } catch (error) {
+        console.error('Error getting follow status:', error);
+        return [];
+    }
+}
