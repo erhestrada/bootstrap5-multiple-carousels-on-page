039a45553e441f2d13eecf52ce9dd7f20b270423
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-16 14:39:24 -0400

    Implement function to get comments on a clip

--- comments/get-comments.js
+++ comments/get-comments.js
@@ -1,3 +1,8 @@
-export default async function getComments() {
+import { makeApiRequest } from "../make-api-request";
 
+export default async function getComments(clipId) {
+    const url = `http://192.168.86.195:3000/clips/${clipId}/comments`;
+    const options = {};
+    const comments = makeApiRequest(url, options);
+    return comments
 }
