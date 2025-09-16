ac274c0bf077ae0baaa2e8cba83cca36c7fd55ce
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-16 15:41:12 -0400

    Implement function to post comment

--- comments/post-comment.js
+++ comments/post-comment.js
@@ -1,3 +1,12 @@
-export default async function postComment() {
+import { makeApiRequest } from "../make-api-request";
 
+export default async function postComment(userId, clipId, parentId, comment) {
+    const url = `http://192.168.86.195:3000/clips/${clipId}/comments`;
+    const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ userId, clipId, parentId, comment }),
+        };
+    const comments = makeApiRequest(url, options);
+    return comments
 }
