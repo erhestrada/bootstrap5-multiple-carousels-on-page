3d366a016f303963a55a4a17be5c6c07c3292acf
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-17 12:10:53 -0400

    Implement function to delete comment from backend

--- comments/delete-comment.js
+++ comments/delete-comment.js
@@ -1,3 +1,12 @@
-export default async function deleteComment() {
+import { makeApiRequest } from "../make-api-request";
 
+export default async function deleteComment(userId, clipId, parentId, comment) {
+    const url = `http://192.168.86.195:3000/clips/${clipId}/comments`;
+    const options = {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ userId, parentId, comment }),
+        };
+    const comments = makeApiRequest(url, options);
+    return comments
 }
