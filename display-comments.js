eea0097339be43e89ce8d5d5b9a391edae6a9d35
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-18 10:43:09 -0400

    Implement cand call function to display number of comments

--- display-comments.js
+++ display-comments.js
@@ -5,6 +5,12 @@ import { renderComments } from "./comment-handlers/comment-handler";
 export async function displayComments(clipId) {
     const comments = await getComments(clipId);
     console.log('comments: ', comments);
+    displayNumberOfComments(comments);
     renderComments(comments);
     return comments;
+}
+
+function displayNumberOfComments(comments) {
+    const commentCountElement = document.getElementById('comment-count');
+    commentCountElement.innerText = comments.length;    
 }
