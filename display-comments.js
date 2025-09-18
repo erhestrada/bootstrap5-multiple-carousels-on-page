c54fb3a5c2f29b491771a45eea60b7f15b87a7db
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-18 09:27:07 -0400

    Implement tentative function to get and display comments for a clip

--- display-comments.js
+++ display-comments.js
@@ -1,3 +1,8 @@
-export async function displayComments() {
+import { getComments } from "./comments";
 
+// Need to do more stuff, like comment-handler
+export async function displayComments() {
+    const comments = await getComments(window.currentClip.id);
+    console.log(comments);
+    return comments;
 }
