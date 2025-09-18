9902b6b50b64420299406c2c6cb86236a1c79863
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-18 09:10:16 -0400

    Add await before makeApiRequest

--- comments/get-comments.js
+++ comments/get-comments.js
@@ -3,6 +3,6 @@ import { makeApiRequest } from "../make-api-request";
 export default async function getComments(clipId) {
     const url = `http://192.168.86.195:3000/clips/${clipId}/comments`;
     const options = {};
-    const comments = makeApiRequest(url, options);
+    const comments = await makeApiRequest(url, options);
     return comments
 }
