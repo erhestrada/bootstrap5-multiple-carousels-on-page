984492703d2b28e9a132d303757d9fe331a7f7c5
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-18 10:10:58 -0400

    Render comments in displayComments

--- display-comments.js
+++ display-comments.js
@@ -1,8 +1,10 @@
 import { getComments } from "./comments";
+import { renderComments } from "./comment-handlers/comment-handler";
 
 // Need to do more stuff, like comment-handler
 export async function displayComments(clipId) {
     const comments = await getComments(clipId);
-    console.log(comments);
+    console.log('comments: ', comments);
+    renderComments(comments);
     return comments;
 }
