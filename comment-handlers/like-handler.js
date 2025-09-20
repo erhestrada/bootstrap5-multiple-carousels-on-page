6391fb79d997bf7692443c3d69a44faa004dd24c
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-20 13:16:15 -0400

    Post and delete like when like is clicked on/off

--- comment-handlers/like-handler.js
+++ comment-handlers/like-handler.js
@@ -1,12 +1,16 @@
+import { postLike, deleteLike } from '../likes';
+
 export function toggleLike(button) {
     const isLiked = button.classList.contains('liked');
     const likeCount = button.querySelector('span');
     let count = parseInt(likeCount.textContent);
     
     if (isLiked) {
+        deleteLike(window.userId, window.currentClip.id, button.dataset.commentId);
         button.classList.remove('liked');
         count--;
     } else {
+        postLike(window.userId, window.currentClip.id, button.dataset.commentId);
         button.classList.add('liked');
         count++;
     }
