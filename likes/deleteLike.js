cc42a0388f3e8e5fb41c34b6d52f4be87191e46f
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-20 13:05:57 -0400

    Impelement API function to delete user like on a comment

---
+++ likes/deleteLike.js
@@ -0,0 +1,8 @@
+import { makeApiRequest } from "../make-api-request";
+
+export default async function deleteLike(userId, clipId, commentId) {
+    const url = `http://192.168.86.195:3000/${userId}/clips/${clipId}/${commentId}/likes`;
+    const options = {};
+    const data = makeApiRequest(url, options);
+    return data;
+}
