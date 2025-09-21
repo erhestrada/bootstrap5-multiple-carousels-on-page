bf2c1d958d3db3e09041463c9e188e254b6b4122
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-21 16:22:25 -0400

    Implement post options object; await makeApiRequest

--- likes/postLike.js
+++ likes/postLike.js
@@ -2,7 +2,11 @@ import { makeApiRequest } from "../make-api-request";
 
 export default async function postLike(userId, clipId, commentId) {
     const url = `http://192.168.86.195:3000/${userId}/clips/${clipId}/${commentId}/likes`;
-    const options = {};
-    const data = makeApiRequest(url, options);
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({})
+    };
+    const data = await makeApiRequest(url, options);
     return data;
 }
