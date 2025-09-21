ed8e3457cf4f94638436d63d5ecc0c3ae93d2c32
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-21 19:04:34 -0400

    Add options object

--- likes/deleteLike.js
+++ likes/deleteLike.js
@@ -2,7 +2,10 @@ import { makeApiRequest } from "../make-api-request";
 
 export default async function deleteLike(userId, clipId, commentId) {
     const url = `http://192.168.86.195:3000/${userId}/clips/${clipId}/${commentId}/likes`;
-    const options = {};
-    const data = makeApiRequest(url, options);
+    const options = {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({})
+    };    const data = makeApiRequest(url, options);
     return data;
 }
