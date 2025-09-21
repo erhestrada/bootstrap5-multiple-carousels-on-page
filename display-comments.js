b670c2b06ff97b79b56f231af79601cefb3b7c71
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-21 16:23:29 -0400

    Remove window.likes, getLikes

--- display-comments.js
+++ display-comments.js
@@ -1,12 +1,10 @@
 import { getComments } from "./comments";
 import { renderComments } from "./comment-handlers/comment-handler";
 
-// Need to do more stuff, like comment-handler
 export async function displayComments(clipId) {
     const comments = await getComments(clipId);
     console.log('comments: ', comments);
     displayNumberOfComments(comments);
-    window.clipComments = comments;
     renderComments();
     return comments;
 }
