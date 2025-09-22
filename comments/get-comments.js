558b9aaf007d81535603dd0873d285387cbf1ded
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-21 22:42:16 -0400

    Add userId query parameter

--- comments/get-comments.js
+++ comments/get-comments.js
@@ -1,7 +1,7 @@
 import { makeApiRequest } from "../make-api-request";
 
-export default async function getComments(clipId) {
-    const url = `http://192.168.86.195:3000/clips/${clipId}/comments`;
+export default async function getComments(clipId, userId) {
+    const url = `http://192.168.86.195:3000/clips/${clipId}/comments?userId=${userId}`;
     const options = {};
     const comments = await makeApiRequest(url, options);
     return comments
