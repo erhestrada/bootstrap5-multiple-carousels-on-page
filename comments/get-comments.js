ec8a2877ba19f35951f5aaea490cc94d2e641f3d
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-23 14:32:49 -0400

    Add missing semicolon

--- comments/get-comments.js
+++ comments/get-comments.js
@@ -4,5 +4,5 @@ export default async function getComments(clipId, userId) {
     const url = `http://192.168.86.195:3000/clips/${clipId}/comments?userId=${userId}`;
     const options = {};
     const comments = await makeApiRequest(url, options);
-    return comments
+    return comments;
 }
