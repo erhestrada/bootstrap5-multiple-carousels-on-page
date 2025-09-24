86bca3913184ba0b21e5b655a1b2285edc7b42b0
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-24 15:25:02 -0400

    Change deleteComment arguments

--- comments/delete-comment.js
+++ comments/delete-comment.js
@@ -1,11 +1,11 @@
 import { makeApiRequest } from "../make-api-request";
 
-export default async function deleteComment(userId, clipId, parentId, comment) {
-    const url = `http://192.168.86.195:3000/clips/${clipId}/comments`;
+export default async function deleteComment(userId, clipId, commentId) {
+    const url = `http://192.168.86.195:3000/clips/${clipId}/comments/${commentId}`;
     const options = {
             method: 'DELETE',
             headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify({ userId, parentId, comment }),
+            body: JSON.stringify({ userId }),
         };
     const response = await makeApiRequest(url, options);
     return response;
