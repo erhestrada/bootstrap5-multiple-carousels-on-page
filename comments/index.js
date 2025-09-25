2ff68a7d908bb9153ecb6c0c1a5d6c12f88e61d5
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-25 15:53:51 -0400

    Replace editComment with softDeleteComment

--- comments/index.js
+++ comments/index.js
@@ -1,4 +1,4 @@
 export { default as getComments } from './get-comments';
 export { default as postComment } from './post-comment';
 export { default as deleteComment } from './delete-comment';
-export { default as editComment } from './soft-delete-comment';
+export { default as softDeleteComment } from './soft-delete-comment';
