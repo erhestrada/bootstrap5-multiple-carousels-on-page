7241f7c2a0bbec6f8af7c1e4d9d715be1b72a9a8
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-25 15:41:07 -0400

    Rename function to softDeleteComment and send data to be patched

--- comments/patch-comment.js
+++ comments/patch-comment.js
@@ -1,3 +1,17 @@
-export default async function editComment() {
+import { makeApiRequest } from "../make-api-request";
 
+export default async function softDeleteComment(clipId, commentId) {
+    const url = `http://192.168.86.195:3000/clips/${clipId}/comments/${commentId}`;
+    const options = {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            user_id: null,
+            comment: '[deleted]',
+            timestamp: null
+        })
+    };
+
+    const response = await makeApiRequest(url, options);
+    return response;
 }
