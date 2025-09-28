473933d07d3746fc267481d1b24085a12f01776a
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-28 16:52:36 -0400

    Calculate number of comments using querySelectorAll instead of nested comments object

--- display-comments.js
+++ display-comments.js
@@ -5,12 +5,17 @@ export async function displayComments(clipId, userId) {
     const comments = await getComments(clipId, userId);
     console.log('comments: ', comments);
     window.clipComments = comments;
-    displayNumberOfComments(comments);
     renderComments();
+    displayNumberOfComments();
     return comments;
 }
 
-function displayNumberOfComments(comments) {
+function displayNumberOfComments() {
     const commentCountElement = document.getElementById('comment-count');
-    commentCountElement.innerText = comments.length;    
+    
+    const comments = document.querySelectorAll('.comment');
+    const replies = document.querySelectorAll('.reply');
+
+    const commentCount = comments.length + replies.length;
+    commentCountElement.innerText = commentCount;    
 }
