02aa65ebf7e632688cbbf102937df0f485f49c8e
Erik <180784081+erhetrada@users.noreply.github.com>
2025-09-29 17:24:44 -0400

    Add if else block to handle both streamers and categories

--- makeFollowingCarousels.js
+++ makeFollowingCarousels.js
@@ -1,12 +1,19 @@
 import { makeNewCarouselForCategory } from './makeNewCarouselForCategory.js';
+import { makeNewCarouselForStreamer } from './makeNewCarouselForStreamer.js';
 
-export async function makeFollowingCarousels() {
+export async function makeFollowingCarousels(categories = false, streamers = false) {
     const categoriesCarousels = document.getElementById('categories-carousels');
     categoriesCarousels.innerHTML = '';
 
-    const followedCategoriesData = window.follows.categories || [];
-
-    followedCategoriesData.forEach(({ category, twitch_id, box_art_url }) => {
-        makeNewCarouselForCategory(category, twitch_id, box_art_url);
-    });
+    if (categories) {
+        const followedCategoriesData = window.follows.categories || [];
+        followedCategoriesData.forEach(({ category, twitch_id, box_art_url }) => {
+            makeNewCarouselForCategory(category, twitch_id, box_art_url);
+        });
+    } else if (streamers) {
+        const followedStreamersData = window.follows.streamers || [];
+        followedStreamersData.forEach(({ streamer, twitch_id }) => {
+            makeNewCarouselForStreamer(streamer, twitch_id);
+        });
+    }
 }
