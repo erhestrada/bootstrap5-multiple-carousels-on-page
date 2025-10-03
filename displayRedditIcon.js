7a4bfe3506affe84b914fb1a66a7d20427671793
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-02 22:11:13 -0400

    Compare clipId of embedUrl with clipIds of reddit posts to find reddit post with clip

--- displayRedditIcon.js
+++ displayRedditIcon.js
@@ -1,3 +1,22 @@
-export function displayRedditIcon(carouselElement, redditPosts) {
+export function checkRedditPosts(embedUrl, redditPosts) {
+    const clipUrl = new URL(embedUrl);
+    const clipId = clipUrl.searchParams.get("clip");
 
+    const matchedPost = redditPosts.find(post => extractClipId(post.linkUrl) === clipId);
+    return matchedPost;
+}
+
+function extractClipId(url) {
+  try {
+    const parsedUrl = new URL(url);
+    const urlParts = parsedUrl.pathname.split('/');
+    const clipIndex = urlParts.indexOf('clip');
+    if (clipIndex !== -1 && urlParts.length > clipIndex + 1) {
+        const clipId = urlParts[clipIndex + 1];
+        return clipId;
+    }
+  } catch {
+    return null;
+  }
+  return null;
 }
