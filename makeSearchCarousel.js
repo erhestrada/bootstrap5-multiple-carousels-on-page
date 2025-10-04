104db53e75fade78952442868de05227e22f9a5c
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-03 22:56:04 -0400

    Display carousel for streamer

--- makeSearchCarousel.js
+++ makeSearchCarousel.js
@@ -1,3 +1,15 @@
-export async function makeSearchCarousel(name, id, thumbnailUrl, type) {
-    alert("wuzzuuuuuuuuuuuuuuup");
+import { makeNewCarouselForCategory } from "./makeNewCarouselForCategory";
+import { makeNewCarouselForStreamer } from "./makeNewCarouselForStreamer";
+
+export async function makeSearchCarousel(name, twitchId, thumbnailUrl, type) {
+    console.log("name", name);
+    console.log("type", type);
+    const daysBack = 1;
+    if (type === "category") {
+        makeNewCarouselForCategory();
+    } else if (type === "streamer") {
+        const categoriesCarousels = document.getElementById('categories-carousels'); // TODO: handle case where streamer has no clips, right now pfp shows then disappears
+        categoriesCarousels.innerHTML = '';
+        makeNewCarouselForStreamer(name, twitchId, thumbnailUrl, daysBack);
+    }
 }
