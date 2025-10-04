4d5e8b4bdbee5cf648393425e3e5b07c6f3c10be
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-03 23:12:54 -0400

    Add TODO comment

--- makeSearchCarousel.js
+++ makeSearchCarousel.js
@@ -1,6 +1,7 @@
 import { makeNewCarouselForCategory } from "./makeNewCarouselForCategory";
 import { makeNewCarouselForStreamer } from "./makeNewCarouselForStreamer";
 
+// TODO: not getting all clips for category? compare to twitch last 24hrs e.g. ffX
 export async function makeSearchCarousel(name, twitchId, thumbnailUrl, type) {
     const categoriesCarousels = document.getElementById('categories-carousels'); // TODO: handle case where streamer has no clips, right now pfp shows then disappears
     categoriesCarousels.innerHTML = '';
