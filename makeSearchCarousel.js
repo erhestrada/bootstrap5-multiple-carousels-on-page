37fd3a73e4c5a8b59934beadb0a0fcdedecf9658
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-03 23:06:02 -0400

    Make category carousel when type is category

--- makeSearchCarousel.js
+++ makeSearchCarousel.js
@@ -2,14 +2,13 @@ import { makeNewCarouselForCategory } from "./makeNewCarouselForCategory";
 import { makeNewCarouselForStreamer } from "./makeNewCarouselForStreamer";
 
 export async function makeSearchCarousel(name, twitchId, thumbnailUrl, type) {
-    console.log("name", name);
-    console.log("type", type);
+    const categoriesCarousels = document.getElementById('categories-carousels'); // TODO: handle case where streamer has no clips, right now pfp shows then disappears
+    categoriesCarousels.innerHTML = '';
+
     const daysBack = 1;
     if (type === "category") {
-        makeNewCarouselForCategory();
+        makeNewCarouselForCategory(name, twitchId, thumbnailUrl);
     } else if (type === "streamer") {
-        const categoriesCarousels = document.getElementById('categories-carousels'); // TODO: handle case where streamer has no clips, right now pfp shows then disappears
-        categoriesCarousels.innerHTML = '';
         makeNewCarouselForStreamer(name, twitchId, thumbnailUrl, daysBack);
     }
 }
