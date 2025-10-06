40b191f4ee36fbc3a892fbdf19c707c8df88f973
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-05 22:30:05 -0400

    Implement function to get all clips a user has interacted with e.g. upvotes, downvotes, favorites, commented on

---
+++ clips/getUserClips.js
@@ -0,0 +1,16 @@
+export default async function getUserClips(userId) {
+    try {
+        const response = await fetch(`http://192.168.86.195:3000/clips/${userId}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const votesData = await response.json();
+        return votesData;
+
+    } catch (error) {
+        console.error('Error getting user clips', error);
+        return null;
+    }
+}
