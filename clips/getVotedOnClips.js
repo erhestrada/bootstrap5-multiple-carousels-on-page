a7dcfb4c84ce20580b018227ed8c8aca5b32c12d
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-06 03:59:12 -0400

    Rename getUserClips to getVotedOnClips

---
+++ clips/getVotedOnClips.js
@@ -0,0 +1,17 @@
+// TODO: Rename to 
+export default async function getVotedOnClips(userId) {
+    try {
+        const response = await fetch(`http://192.168.86.195:3000/clips/${userId}/votes`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const votesData = await response.json();
+        return votesData;
+
+    } catch (error) {
+        console.error('Error getting user clips', error);
+        return null;
+    }
+}
