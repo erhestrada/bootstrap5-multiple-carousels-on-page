a29d3a482b67ec80c7b431d6f163a17a0adbea8a
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-06 03:53:10 -0400

    Generalize extractClipId to handle alternative twitch url format

--- displayRedditIcon.js
+++ displayRedditIcon.js
@@ -2,6 +2,8 @@ export function checkRedditPosts(embedUrl, redditPosts) {
     const clipUrl = new URL(embedUrl);
     const clipId = clipUrl.searchParams.get("clip");
 
+    // https://www.twitch.tv/robcdee/clip/IncredulousSmokyLaptopDBstyle-bLEuqGl1hFGyyfgt
+    // https://clips.twitch.tv/FrigidPoliteKathyShadyLulu-jizZPTJxGwgYMGH2
     const matchedPost = redditPosts.find(post => extractClipId(post.linkUrl) === clipId);
     return matchedPost;
 }
@@ -9,14 +11,22 @@ export function checkRedditPosts(embedUrl, redditPosts) {
 function extractClipId(url) {
   try {
     const parsedUrl = new URL(url);
-    const urlParts = parsedUrl.pathname.split('/');
-    const clipIndex = urlParts.indexOf('clip');
-    if (clipIndex !== -1 && urlParts.length > clipIndex + 1) {
-        const clipId = urlParts[clipIndex + 1];
-        return clipId;
+    const pathParts = parsedUrl.pathname.split('/').filter(Boolean); // Filter out empty strings e.g. ["", "..."] from "/..."
+
+    // Format 1: twitch.tv/channel/clip/SmokyLaptop
+    const clipIndex = pathParts.indexOf('clip');
+    if (clipIndex !== -1 && pathParts.length > clipIndex + 1) {
+      return pathParts[clipIndex + 1];
     }
+
+    // Format 2: clips.twitch.tv/SmokyLaptop
+    if (parsedUrl.hostname === 'clips.twitch.tv' && pathParts.length >= 1) {
+      return pathParts[0];
+    }
+
   } catch {
     return null;
   }
+
   return null;
 }
