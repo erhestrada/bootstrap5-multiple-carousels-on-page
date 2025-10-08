7683db4e061510ddea378a0e5b6965eca24dbcba
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-08 01:27:35 -0400

    Export getHistoryClips

--- clips/index.js
+++ clips/index.js
@@ -1,3 +1,4 @@
 export { default as getVotedOnClips } from './getVotedOnClips';
 export { default as getFavoritedClips } from './get-favorited-clips';
 export { default as getCommentedOnClips } from './getCommentedOnClips';
+export { default as getHistoryClips } from './getHistoryClips';
