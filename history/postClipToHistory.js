c913005352635ab21859c6f632944adb96c9644e
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-08 00:20:17 -0400

    Implement function to insert clip into history table

---
+++ history/postClipToHistory.js
@@ -0,0 +1,21 @@
+// clipId: twitch_id for clip
+export default async function postClipToHistory(userId, clipId) {
+    try {
+        const response = await fetch('http://192.168.86.195:3000/history', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ userId, clipId})
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const voteId = await response.json();
+        return voteId;
+    
+    } catch(error) {
+        console.error("Error inserting clip into history table in database", error);
+        throw new Error(`Error inserting clip into history table in database: ${error.message}`);
+    }
+}
