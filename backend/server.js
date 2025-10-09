659bc6d0de9a13fce7b7725098b6bbfe8ed90048
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-09 12:42:34 -0400

    Change twitch_id to TEXT

--- backend/server.js
+++ backend/server.js
@@ -59,8 +59,8 @@ db.serialize(() => {
   FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE)
     `);
 
-  db.run('CREATE TABLE IF NOT EXISTS followed_streamers (id INTEGER PRIMARY KEY, user_id INTEGER, streamer TEXT, twitch_id INTEGER, profile_picture_url TEXT, FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE)');
-  db.run('CREATE TABLE IF NOT EXISTS followed_categories (id INTEGER PRIMARY KEY, user_id INTEGER, category TEXT, twitch_id INTEGER, box_art_url TEXT, FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE)');
+  db.run('CREATE TABLE IF NOT EXISTS followed_streamers (id INTEGER PRIMARY KEY, user_id INTEGER, streamer TEXT, twitch_id TEXT, profile_picture_url TEXT, FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE)');
+  db.run('CREATE TABLE IF NOT EXISTS followed_categories (id INTEGER PRIMARY KEY, user_id INTEGER, category TEXT, twitch_id TEXT, box_art_url TEXT, FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE)');
 
   db.run('CREATE TABLE IF NOT EXISTS clips (id INTEGER PRIMARY KEY, twitchId TEXT UNIQUE, url TEXT, embed_url TEXT, broadcaster_id TEXT, broadcaster_name TEXT, creator_id TEXT, creator_name TEXT, video_id TEXT, game_id TEXT, language TEXT, title TEXT, view_count INTEGER, created_at TEXT, thumbnail_url TEXT, duration INTEGER)');
 });
@@ -672,7 +672,7 @@ app.get('/users/:id/following', (req, res) => {
 
       const formattedCategoryRows = categoryRows.map(row => ({
         category: row.category,
-        categoryId: row.twitch_id.toString(),
+        categoryId: row.twitch_id,
         boxArtUrl: row.box_art_url
       }));
 
