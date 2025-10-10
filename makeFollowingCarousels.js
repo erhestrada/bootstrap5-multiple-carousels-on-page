25d370abc744f1622149421b961c76d3dcae5aa7
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-10 13:39:48 -0400

    Change twitchId to categoryId

--- makeFollowingCarousels.js
+++ makeFollowingCarousels.js
@@ -1,14 +1,17 @@
 import { makeNewCarouselForCategory } from './makeNewCarouselForCategory.js';
 import { makeNewCarouselForStreamer } from './makeNewCarouselForStreamer.js';
 
-export async function makeFollowingCarousels({ categories = false, streamers = false } = {}) {
+export async function makeFollowingCarousels({ categories = false, streamers = false, container = null } = {}) {
     const categoriesCarousels = document.getElementById('categories-carousels');
     categoriesCarousels.innerHTML = '';
 
     if (categories) {
         const followedCategoriesData = window.follows.categories || [];
-        followedCategoriesData.forEach(({ category, twitchId, boxArtUrl }) => {
-            makeNewCarouselForCategory(category, twitchId, boxArtUrl, categoriesCarousels);
+        console.log("heyo");
+        console.log(followedCategoriesData);
+        followedCategoriesData.forEach(({ category, categoryId, boxArtUrl }) => {
+            console.log(category, categoryId);
+            makeNewCarouselForCategory(category, categoryId, boxArtUrl, container);
         });
     } else if (streamers) {
         const followedStreamersData = window.follows.streamers || [];
