aa5efbf3c408eb4325a611f2a9dfb87f070b6572
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-10 13:59:55 -0400

    Remove categoriesContainer content reset

--- makeFollowingCarousels.js
+++ makeFollowingCarousels.js
@@ -2,15 +2,9 @@ import { makeNewCarouselForCategory } from './makeNewCarouselForCategory.js';
 import { makeNewCarouselForStreamer } from './makeNewCarouselForStreamer.js';
 
 export async function makeFollowingCarousels({ categories = false, streamers = false, container = null } = {}) {
-    const categoriesCarousels = document.getElementById('categories-carousels');
-    categoriesCarousels.innerHTML = '';
-
     if (categories) {
         const followedCategoriesData = window.follows.categories || [];
-        console.log("heyo");
-        console.log(followedCategoriesData);
         followedCategoriesData.forEach(({ category, categoryId, boxArtUrl }) => {
-            console.log(category, categoryId);
             makeNewCarouselForCategory(category, categoryId, boxArtUrl, container);
         });
     } else if (streamers) {
