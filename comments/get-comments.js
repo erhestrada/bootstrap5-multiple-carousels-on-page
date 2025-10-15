6b903473514452fd0b5e1d167bac32ba68568555
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-15 18:27:21 -0400

    Make path unique to avoid path conflicts

--- comments/get-comments.js
+++ comments/get-comments.js
@@ -1,7 +1,8 @@
 import { makeApiRequest } from "../make-api-request";
 
 export default async function getComments(clipId, userId) {
-    const url = `http://192.168.86.195:3000/clips/${clipId}/comments?userId=${userId}`;
+    // TODO: refactor path
+    const url = `http://192.168.86.195:3000/abc/clips/${clipId}/comments?userId=${userId}`;
     const options = {};
     const comments = await makeApiRequest(url, options);
     return comments;
