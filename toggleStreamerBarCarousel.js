174ad8657863b0f71722deaffb40a3740c492a8a
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-17 18:06:25 -0400

    Expand currently playing clip

--- toggleStreamerBarCarousel.js
+++ toggleStreamerBarCarousel.js
@@ -1,4 +1,9 @@
 export function toggleStreamerBarCarousel() {
     const streamerBarCarousel = document.getElementById('id-streamer-bar-carousel-carousel-row');
     streamerBarCarousel.style.display = 'none';
+
+    const iframeContainer = document.getElementById('iframe-container');
+    const iframe = iframeContainer.querySelector('iframe');
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
 }
