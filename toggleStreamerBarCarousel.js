2395a62374208c68aeedf1f7d561dda3da14f6d3
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-17 18:02:10 -0400

    Hide streamer bar carousel

--- toggleStreamerBarCarousel.js
+++ toggleStreamerBarCarousel.js
@@ -1,3 +1,4 @@
 export function toggleStreamerBarCarousel() {
-
+    const streamerBarCarousel = document.getElementById('id-streamer-bar-carousel-carousel-row');
+    streamerBarCarousel.style.display = 'none';
 }
