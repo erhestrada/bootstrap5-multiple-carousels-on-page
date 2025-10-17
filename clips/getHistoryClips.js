f62b1951a5e239bafa243fc85d1cea990feceaa7
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-17 15:13:26 -0400

    Filter history clips to remove consecutive duplicates that might arise from refreshing

--- clips/getHistoryClips.js
+++ clips/getHistoryClips.js
@@ -8,7 +8,10 @@ export default async function getHistoryClips(userId) {
 
         const historyClips = await response.json();
         console.log("history clips: ", historyClips);
-        return historyClips;
+        const noConsecutiveDuplicatesHistoryClips = historyClips.filter((clip, index, clips) => {  // filter out consecutive duplicate clips to handle refresh spammming
+            return index === 0 || clip.history_clip_id !== clips[index - 1].history_clip_id;       // index === 0 handles first clip
+        });
+        return noConsecutiveDuplicatesHistoryClips;
 
     } catch (error) {
         console.error('Error getting history clips', error);
