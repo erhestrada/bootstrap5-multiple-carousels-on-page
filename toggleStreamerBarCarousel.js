c18f04aece7d2a0097bd71e185ed5078b1bad353
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-17 18:45:07 -0400

    Set tentative larger size for clip

--- toggleStreamerBarCarousel.js
+++ toggleStreamerBarCarousel.js
@@ -4,6 +4,6 @@ export function toggleStreamerBarCarousel() {
 
   const iframeContainer = document.getElementById('iframe-container');
   const iframe = iframeContainer.querySelector('iframe');
-  iframe.style.width = '100%';
-  iframe.style.height = '100%';
+  iframe.style.width = '928px';
+  iframe.style.height = '522px';  // somewhere between 854-480 and 960-540
 }
