10ef7c1fcec7804c246ad06ecb3a33204b9211b1
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-18 15:07:27 -0400

    Toggle .enlarged-video on iframe for currently playing clip to enlarge or shrink it

--- toggleStreamerBarCarousel.js
+++ toggleStreamerBarCarousel.js
@@ -4,6 +4,5 @@ export function toggleStreamerBarCarousel() {
 
   const iframeContainer = document.getElementById('iframe-container');
   const iframe = iframeContainer.querySelector('iframe');
-  iframe.style.width = '928px';
-  iframe.style.height = '522px';  // somewhere between 854-480 and 960-540
+  iframe.classList.toggle('enlarged-video');
 }
