2a80c463454af231d163b9539f318a6eb9d514aa
Erik <180784081+erhetrada@users.noreply.github.com>
2025-10-18 16:37:29 -0400

    Set size on iframe container so when iframe becomes absolutely positioned container doesn't collapse and cut off the iframe

--- toggleStreamerBarCarousel.js
+++ toggleStreamerBarCarousel.js
@@ -3,6 +3,9 @@ export function toggleStreamerBarCarousel() {
   streamerBarCarousel.classList.toggle('hidden');
 
   const iframeContainer = document.getElementById('iframe-container');
+  iframeContainer.style.position = 'relative';
+  iframeContainer.style.width = '928px';
+  iframeContainer.style.height = '522px';
   const iframe = iframeContainer.querySelector('iframe');
   iframe.classList.toggle('enlarged-video');
 }
