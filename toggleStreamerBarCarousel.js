eebaa70927d89146fbd3ab3d0cacce95c4ffa9ee
Erik <180784081+erhetrada@users.noreply.github.com>
2025-11-01 01:20:51 -0400

    Reset video size when video not enlarged

--- toggleStreamerBarCarousel.js
+++ toggleStreamerBarCarousel.js
@@ -1,32 +1,17 @@
 export function toggleStreamerBarCarousel() {
-  //const historyContainer = document.getElementById('history-container');
-  //historyContainer.classList.toggle('hidden');
-
-  //const commentsSection = document.getElementById('comments-section');
-  //commentsSection.classList.toggle('hidden');
-
   const streamerBar = document.querySelector('.streamer-bar');
   streamerBar.classList.toggle('hidden');
 
-  const iframe = document.querySelector('iframe'); // select the iframe
+  const iframeContainer = document.getElementById('iframe-container');
+  const iframe = iframeContainer.querySelector('iframe');
+  iframe.classList.toggle('enlarged-video');
 
   iframe.style.width = "90%";
   iframe.style.height = "auto";
   iframe.style.aspectRatio = "16 / 9";
 
-  /*
-  const iframeContainer = document.getElementById('iframe-container');
-  const iframe = iframeContainer.querySelector('iframe');
-  iframe.classList.toggle('enlarged-video');
-
-  if (iframe.classList.contains('enlarged-video')) {
-    iframeContainer.style.position = 'relative';
-    iframeContainer.style.width = '928px';
-    iframeContainer.style.height = '522px';
-  } else {
-    iframeContainer.style.position = '';
-    iframeContainer.style.width = '';
-    iframeContainer.style.height = '';
+  if (!iframe.classList.contains('enlarged-video')) {
+    iframe.style.height = "360px";
+    iframe.style.width = "640px";
   }
-  */
 }
