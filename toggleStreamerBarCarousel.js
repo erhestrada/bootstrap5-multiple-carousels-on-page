77bd45a26abf2338515d9dbc0a43376a78180326
Erik <180784081+erhetrada@users.noreply.github.com>
2025-11-01 23:03:56 -0400

    Refactor to use default-video-size class

--- toggleStreamerBarCarousel.js
+++ toggleStreamerBarCarousel.js
@@ -4,14 +4,6 @@ export function toggleStreamerBarCarousel() {
 
   const iframeContainer = document.getElementById('iframe-container');
   const iframe = iframeContainer.querySelector('iframe');
-  iframe.classList.toggle('enlarged-video');
-
-  iframe.style.width = "90%";
-  iframe.style.height = "auto";
-  iframe.style.aspectRatio = "16 / 9";
-
-  if (!iframe.classList.contains('enlarged-video')) {
-    iframe.style.height = "360px";
-    iframe.style.width = "640px";
-  }
+  iframe.classList.toggle('enlarged-video'); // Off at start so first toggle switches it on
+  iframe.classList.toggle('default-video-size'); // On at start so first toggle switches it off
 }
