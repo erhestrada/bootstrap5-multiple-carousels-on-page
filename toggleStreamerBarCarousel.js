fad7157fd68894fcd18c375dad86346726664428
Erik <180784081+erhetrada@users.noreply.github.com>
2025-11-02 00:48:10 -0400

    Append elements to theater mode containers

--- toggleStreamerBarCarousel.js
+++ toggleStreamerBarCarousel.js
@@ -6,4 +6,25 @@ export function toggleStreamerBarCarousel() {
   const iframe = iframeContainer.querySelector('iframe');
   iframe.classList.toggle('enlarged-video'); // Off at start so first toggle switches it on
   iframe.classList.toggle('default-video-size'); // On at start so first toggle switches it off
+
+  const pfp = document.querySelector('.pfp-image'); 
+  const changeCarouselStuff = document.getElementById('change-carousel-buttons-container');
+
+  const prevButton = document.getElementById('clip-player-prev-btn');
+  const nextButton = document.getElementById('clip-player-next-btn');
+
+  if (iframe.classList.contains('enlarged-video')) {
+    const pfpContainer = document.getElementById('theater-mode-pfp-container');
+    pfpContainer.appendChild(prevButton);
+    pfpContainer.appendChild(pfp);
+
+    const changeContainer = document.getElementById('theater-mode-change-carousel-buttons-container');
+    changeContainer.appendChild(nextButton);
+    changeContainer.appendChild(changeCarouselStuff);
+  } else {
+    const streamerBar = document.querySelector('.streamer-bar');
+    streamerBar.prepend(pfp);
+    streamerBar.appendChild(changeCarouselStuff);
+
+  }
 }
