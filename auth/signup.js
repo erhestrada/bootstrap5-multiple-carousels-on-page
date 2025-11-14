75d6110576411813bf95d9a0908cfc76887d45f6
Erik <180784081+erhetrada@users.noreply.github.com>
2025-11-14 02:34:33 -0500

    Add username and password arguments to signup function

--- auth/signup.js
+++ auth/signup.js
@@ -1,3 +1,5 @@
-export default async function signup() {
+// db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, client_id TEXT, username TEXT UNIQUE, password TEXT)');
+
+export default async function signup(username, password) {
     alert('sign up');
 }
