222e4c6316872b8ac8d855fe830221abc8aebd64
Erik <180784081+erhetrada@users.noreply.github.com>
2025-11-14 06:39:16 -0500

    Implement request function to get all users

--- auth/signup.js
+++ auth/signup.js
@@ -1,5 +1,28 @@
 // db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, client_id TEXT, username TEXT UNIQUE, password TEXT)');
-
+// get users
+// check if username novel else error
+// if valid username post
 export default async function signup(username, password) {
+    const users = await getUsers();
     alert('sign up');
+}
+
+// move to /users ?
+export async function getUsers() {
+    try {
+        const url = `http://192.168.86.195:3000/users/`;
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const users = await response.json();
+        console.log('users: ', users);
+        return users;
+
+    } catch (error) {
+        console.error('Error getting users', error);
+        return [];
+    }
 }
