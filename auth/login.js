2b35f099f324e62b548017ef727f345b6e394ea2
Erik <180784081+erhetrada@users.noreply.github.com>
2025-11-23 00:36:13 -0500

    Get users and initialize if-else block for handling possibilities

--- auth/login.js
+++ auth/login.js
@@ -1,3 +1,14 @@
+import { getUsers } from "./signup";
+
 export default async function login(username, password) {
+    const users = await getUsers();
+    
+    if (users.contains(username)) {
+        // if password is correct
+        // else if password is incorrect
+    } else {
+        // username does not exist message
+    }
     alert('log in');
 }
+
