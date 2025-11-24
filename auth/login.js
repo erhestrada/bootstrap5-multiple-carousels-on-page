397c6f205c139233c9e88987ed9289de66023dfa
Erik <180784081+erhetrada@users.noreply.github.com>
2025-11-24 00:29:16 -0500

    Display error message when logging in with a username that doesn't exist

--- auth/login.js
+++ auth/login.js
@@ -1,13 +1,20 @@
 import { getUsers } from "./signup";
 
+// TODO: change usernameTakeMessage to something more general like usernameErrorMessage - change evewrywhere
+// in x click handler make sure to hide error message (resetting modal)
+
 export default async function login(username, password) {
     const users = await getUsers();
+    const usernameTakenMessage = document.getElementById("username-taken-message");
     
-    if (users.contains(username)) {
+    if (users.some(user => user.username === username)) {
         // if password is correct
         // else if password is incorrect
     } else {
         // username does not exist message
+        usernameTakenMessage.innerText = "This username does NOT exist!";
+        usernameTakenMessage.classList.remove('hidden');
+        console.error('Failed to update login information:', error.message);
     }
     alert('log in');
 }
