5708b9280cdf08b9edc0eeec0aae6980f9cdc693
Erik <180784081+erhetrada@users.noreply.github.com>
2025-11-27 01:34:33 -0500

    Rename variable from usernameTakenMessage to usernameErrorMessage

--- auth/login.js
+++ auth/login.js
@@ -2,15 +2,15 @@ import { getUsers, displayUsername } from "./signup";
 
 export default async function login(username, password) {
     const users = await getUsers();
-    const usernameTakenMessage = document.getElementById("username-error-message");
+    const usernameErrorMessage = document.getElementById("username-error-message");
     
     // if password is correct
     if (users.some(user => user.username === username)) {
         displayUsername();
     } else {
         // username does not exist message
-        usernameTakenMessage.innerText = "This username does NOT exist!";
-        usernameTakenMessage.classList.remove('hidden');
+        usernameErrorMessage.innerText = "This username does NOT exist!";
+        usernameErrorMessage.classList.remove('hidden');
         console.error('Failed to update login information:', error.message);
     }
     alert('log in');
