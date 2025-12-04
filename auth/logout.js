329d0c8eaf180a883279df4d7f4d5d1ea49da4bb
Erik <180784081+erhetrada@users.noreply.github.com>
2025-12-04 13:57:31 -0500

    Change login button text back to Log In after successful logout

--- auth/logout.js
+++ auth/logout.js
@@ -5,4 +5,7 @@ export default async function logout() {
 function hideUsername() {
     const profileLinkElement = document.getElementById('profile-btn');
     profileLinkElement.textContent = 'Profile';
+
+    const navbarLoginButton = document.getElementById('log-in-button');
+    navbarLoginButton.innerText = "Log in";
 }
