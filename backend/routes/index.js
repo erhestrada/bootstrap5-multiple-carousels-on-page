45b2cb1d9375f03e52987ed0be14c5c1ff997f6c
Erik <180784081+erhetrada@users.noreply.github.com>
2026-01-05 14:41:53 -0500

    Export routers

---
+++ backend/routes/index.js
@@ -0,0 +1,4 @@
+export { default as clipsRouter } from './clips';
+export { default as favoritesRouter } from './favorites';
+export { default as usersRouter } from './useres';
+export { default as votesRouter } from './votes';
