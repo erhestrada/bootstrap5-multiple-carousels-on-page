078c66fc4a9f5d0197f6499aef5990c0d402d672
Erik <180784081+erhetrada@users.noreply.github.com>
2026-01-06 20:15:27 -0500

    Import from barrel file

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,7 @@ import cors from 'cors';
 import sqlite3Pkg from 'sqlite3';
 import { getRedditPosts } from './getRedditPosts.js';
 import { getTwitchAcessToken } from './getTwitchAccessToken.js';
-import clipsRouter from './routes/clips.js';
-import votesRouter from './routes/votes.js'; // TODO: make barrel file
-import favoritesRouter from './routes/favorites.js';
-import usersRouter from './users.js';
+import { clipsRouter, votesRouter, favoritesRouter, usersRouter }from './routes';
 
 // TODO: connect twitch authtoken from token.json to auth token in ../config.js - the one i'm actually using
 // TODO: run getTwitchAccessToken separately from when server starts
