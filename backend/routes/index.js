1d6686ec2ed4c5d6ece80201c93fb7fd9065fb28
Erik <180784081+erhetrada@users.noreply.github.com>
2026-01-08 23:25:44 -0500

    Fix typo

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,4 @@
 export { default as clipsRouter } from './clips';
 export { default as favoritesRouter } from './favorites';
-export { default as usersRouter } from './useres';
+export { default as usersRouter } from './users';
 export { default as votesRouter } from './votes';
