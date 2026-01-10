beed0fc8b0e2ae620977713407cda6bb6a8210f6
Erik <180784081+erhetrada@users.noreply.github.com>
2026-01-10 17:24:05 -0500

    Make dedicated file for getAllRowsFromTable

---
+++ backend/utils/getAllRowsFromTable.js
@@ -0,0 +1,8 @@
+function getAllRowsFromTable(tableName, res) {
+  const query = `SELECT * FROM ${tableName}`;
+
+  db.all(query, [], (err, rows) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(rows);
+  });
+}
