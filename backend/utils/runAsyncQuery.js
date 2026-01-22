1e1b3b7db8530eeae3bf4adb8e5191cb4e0593ea
Erik <180784081+erhetrada@users.noreply.github.com>
2026-01-21 23:23:40 -0500

    Refactor runAsyncQuery to take db as an argument

--- backend/utils/runAsyncQuery.js
+++ backend/utils/runAsyncQuery.js
@@ -1,4 +1,4 @@
-export function runAsyncQuery(query, parameters) {
+export function runAsyncQuery(db, query, parameters) {
   return new Promise((resolve, reject) => {
     db.all(query, parameters, (err, rows) => {
       if (err) return reject(err);
