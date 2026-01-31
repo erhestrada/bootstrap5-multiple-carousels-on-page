b51f4f6dc84b3e4a08c901de5217938a86bf12ec
Erik <180784081+erhestrada@users.noreply.github.com>
2026-01-31 10:59:50 -0500

    Move getAllRowsFromTable to utils.js

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -52,3 +52,12 @@ export function dbRunAsync(db, query, params) {
     });
   });
 }
+
+export function getAllRowsFromTable(tableName, res) {
+  const query = `SELECT * FROM ${tableName}`;
+
+  db.all(query, [], (err, rows) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(rows);
+  });
+}
