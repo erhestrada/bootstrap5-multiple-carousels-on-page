7b8ac8cf51dc2d6726548bae465a66781b2e4b62
Erik <180784081+erhestrada@users.noreply.github.com>
2026-01-31 11:06:15 -0500

    Delete runAsyncQuery.js

--- backend/utils/runAsyncQuery.js
+++
@@ -1,8 +0,0 @@
-export function runAsyncQuery(db, query, parameters) {
-  return new Promise((resolve, reject) => {
-    db.all(query, parameters, (err, rows) => {
-      if (err) return reject(err);
-      resolve(rows);
-    });
-  });
-}
