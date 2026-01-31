619c9f242fb908a40a4feff4e9c65884eb07de61
Erik <180784081+erhestrada@users.noreply.github.com>
2026-01-31 11:06:51 -0500

    Delete getAllRowsFromTable.js

--- backend/utils/getAllRowsFromTable.js
+++
@@ -1,8 +0,0 @@
-export function getAllRowsFromTable(tableName, res) {
-  const query = `SELECT * FROM ${tableName}`;
-
-  db.all(query, [], (err, rows) => {
-    if (err) return res.status(500).json({ error: err.message });
-    res.json(rows);
-  });
-}
