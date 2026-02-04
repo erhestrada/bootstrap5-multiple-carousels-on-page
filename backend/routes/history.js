7ab0a5dfd1755443f2a18295b49cdbcc869fb9f9
Erik <180784081+erhestrada@users.noreply.github.com>
2026-02-03 20:48:22 -0500

    Copy history route from server.js

--- backend/routes/history.js
+++ backend/routes/history.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import { insertRowIntoTable } from '../utils/utils.js';
+
 const historyRouter = Router();
 
-historyRouter.get('/', (req, res) => {
-    res.send({message: "History endpoint hit"});
+historyRouter.post('/', (req, res) => {
+  const { userId, clipTwitchId: twitchId } = req.body;
+
+  // const query = 'INSERT INTO favorites (user_id, clip_id) VALUES (?, ?)';
+  const tableName = 'history';
+  const columnNames = ['user_id', 'twitch_id'];
+  const parameters = [userId, twitchId];
+
+  insertRowIntoTable(tableName, columnNames, parameters, res);
 });
 
 export default historyRouter;
-
