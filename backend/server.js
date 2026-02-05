57bab4d1ada40f4ef402d3a912f1ce600b4a1cd9
Erik <180784081+erhestrada@users.noreply.github.com>
2026-02-04 20:46:37 -0500

    Remove users endpoints implemented in users router

--- backend/server.js
+++ backend/server.js
@@ -33,55 +33,8 @@ const db = new sqlite3.Database('./data.db');
 initializeDb(db);
 
 app.use("/clips", clipsRouter);
-app.use("/users", usersRouter);
+app.use("/users", usersRouter); // TODO: test patch user/login
 app.use("/history", historyRouter);
-// ---------------------------- Users ------------------------------ TODO: need to change endpoints on the frontend so they all begin with /users, then i can replace with app.use
-// Get all users
-app.get('/users', (req, res) => {
-  getAllRowsFromTable('users', res);
-});
-
-app.get('/users/signed-out-user', async (req, res) => {
-  const { clientId } = req.query;
-
-  if (!clientId) {
-    return res.status(400).json({ error: 'Missing clientId' });
-  }
-
-  try {
-    const { userId, username } = await getSignedOutUserId(clientId);
-    res.status(200).json({ userId, username });
-  } catch (err) {
-    console.error('Failed to get signed out user:', err);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-// Update user username and password
-app.patch('/:userId/login', (req, res) => {
-  const { userId } = req.params;
-  const { username, password } = req.body;
-
-  const updateQuery = `
-    UPDATE users
-    SET username = ?, password = ?
-    WHERE id = ?
-  `;
-  const parameters = [username, password, userId];
-
-  db.run(updateQuery, parameters, function (err) {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-
-    if (this.changes === 0) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-
-    console.log(`User: ${userId} username and password updated successfully. New username: ${username}`);
-    res.json({ message: 'Login updated successfully' });
-  });
-});
 // ---------------------------- Clips ------------------------------
 app.get('/clips/:userId/votes', (req, res) => {
   const userId = req.params.userId;
