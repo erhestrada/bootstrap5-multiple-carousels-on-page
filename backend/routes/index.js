e514d3d4066bbec55aa47f53c806c198cf97ee2b
Erik <180784081+erhestrada@users.noreply.github.com>
2026-02-07 14:35:47 -0500

    Export commentsRouter

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,3 +3,4 @@ export { default as favoritesRouter } from './favorites.js';
 export { default as usersRouter } from './users.js';
 export { default as votesRouter } from './votes.js';
 export { default as historyRouter } from './history.js';
+export { default as commentsRouter } from './comments.js';
