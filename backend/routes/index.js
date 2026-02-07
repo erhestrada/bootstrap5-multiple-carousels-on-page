a5931338cb7d931647fe0a7365460e4c499bd303
Erik <180784081+erhestrada@users.noreply.github.com>
2026-02-06 23:45:31 -0500

    Export historyRouter

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,3 +2,4 @@ export { default as clipsRouter } from './clips.js';
 export { default as favoritesRouter } from './favorites.js';
 export { default as usersRouter } from './users.js';
 export { default as votesRouter } from './votes.js';
+export { default as historyRouter } from './history.js';
