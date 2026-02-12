8dd9ca42ddf8504bf0519afec2203b990d36fc55
Erik <180784081+erhestrada@users.noreply.github.com>
2026-02-11 23:45:06 -0500

    Add comments endpoints

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -2,7 +2,12 @@ const API_BASE = '';
 
 export const api = {
 	clips: [],
-	comments: [],
+	comments: {'activity': '/:userId/activity',
+			'getComments': '/abc/clips/:clipId/comments',
+			'postComment': '/clips/:clipId/comments', 
+			'deleteComment': '/clips/:clipId/comments/:commentId',
+			'patchComment': '/clips/:clipId/comments/:commentId'
+	}
 	favorites: [],
 	history: [],
 	likes: [],
