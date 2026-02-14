0afd7d4a0a82335edd4876f0f18a08f0adb11bb0
Erik <180784081+erhestrada@users.noreply.github.com>
2026-02-13 23:35:27 -0500

    Remove favorites routes

--- backend/server.js
+++ backend/server.js
@@ -60,56 +60,6 @@ app.delete('/:userId/clips/:clipId/:commentId/likes', (req, res) => {
   deleteRowFromTable(tableName, columnNames, parameters, res);
 });
 
-// ---------------------------- Favorites ------------------------------
-app.get('/:userId/favorites', (req, res) => {
-  const userId = req.params.userId;
-  const tableName = 'favorites';
-  const columnName = 'user_id';
-  const filterValue = userId;
-  getValueFilteredDataFromTable(tableName, columnName, filterValue, res);
-});
-
-// Get favorite status of clip
-app.get('/favorites/:userId/:clipId', (req, res) => {
-  const { userId, clipId } = req.params;
-
-  const query = `SELECT * FROM favorites WHERE user_id = ? AND clip_id = ?`;
-
-  db.get(query, [userId, clipId], (err, row) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-
-    if (!row) {
-      return res.json({ favorited: false });
-    }
-
-    res.json({ favorited: true });
-  });
-});
-
-app.post('/favorites', (req, res) => {
-  const { userId, clipId } = req.body;
-
-  // const query = 'INSERT INTO favorites (user_id, clip_id) VALUES (?, ?)';
-  const tableName = 'favorites';
-  const columnNames = ['user_id', 'clip_id'];
-  const parameters = [userId, clipId];
-
-  insertRowIntoTable(tableName, columnNames, parameters, res);
-});
-
-app.delete('/favorites', (req, res) => {
-  const { userId, clipId } = req.body;
-
-  //const query = 'DELETE FROM favorites WHERE user_id = ? AND clip_id = ?';
-  const tableName = 'favorites';
-  const columnNames = ['user_id', 'clip_id'];
-  const parameters = [userId, clipId];
-
-  deleteRowFromTable(tableName, columnNames, parameters, res);
-});
-
 // ---------------------------- Clips ------------------------------
 
 app.post('/clips', (req, res) => {
