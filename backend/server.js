bead9fea1d0a4232d2c32e5e3046e8d76cb1dfaf
Erik <180784081+erhestrada@users.noreply.github.com>
2026-02-15 23:13:26 -0500

    Remove likes routes

--- backend/server.js
+++ backend/server.js
@@ -37,30 +37,6 @@ app.use("/users", usersRouter); // TODO: test patch user/login
 app.use("/history", historyRouter);
 app.use("/comments", commentsRouter);
 app.use("/favorites", favoritesRouter);
-// ---------------------------- Likes ------------------------------
-
-// Post like
-app.post('/:userId/clips/:clipId/:commentId/likes', (req, res) => {
-  const { userId, commentId } = req.params;
-
-  const tableName = 'comment_likes';
-  const columnNames = ['user_id', 'comment_id'];
-  const parameters = [userId, commentId];
-
-  insertRowIntoTable(tableName, columnNames, parameters, res);
-});
-
-// Delete like
-app.delete('/:userId/clips/:clipId/:commentId/likes', (req, res) => {
-  const { userId, commentId } = req.params;
-
-  const tableName = 'comment_likes';
-  const columnNames = ['user_id', 'comment_id'];
-  const parameters = [userId, commentId];
-
-  deleteRowFromTable(tableName, columnNames, parameters, res);
-});
-
 // ---------------------------- Clips ------------------------------
 
 app.post('/clips', (req, res) => {
