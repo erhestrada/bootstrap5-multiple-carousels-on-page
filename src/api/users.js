1cc67928d5a666ed1d5be6bee51abb87e3c60fab
Erik <180784081+erhestrada@users.noreply.github.com>
2026-03-05 19:14:53 -0500

    Consolidate users api functions into users.js

---
+++ src/api/users.js
@@ -0,0 +1,32 @@
+export async function postUser(uuid) {
+    try {
+        const response = await fetch('http://192.168.86.195:3000/postUser', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ uuid })
+        });
+        const result = await response.json();
+        console.log('Data Stored:', result);
+
+    } catch (error) {
+        console.error('Error storing data:', error);
+    }
+};
+// Rename to getSignedOutUserId
+export default async function getSignedOutUser(clientId) {
+    try {
+        const response = await fetch(`http://192.168.86.195:3000/users/signed-out-user?clientId=${clientId}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const userData = await response.json();
+        const { userId, username } = userData;
+        return { userId, username };
+
+    } catch (error) {
+        console.error('Error loading userId', error);
+        return null;
+    }
+}
