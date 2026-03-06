5937070cb94aa8d05cc45a36f4796ba13c314a88
Erik <180784081+erhestrada@users.noreply.github.com>
2026-03-06 18:48:48 -0500

    Consolidate likes api functions into likes.js

---
+++ src/api/likes.js
@@ -0,0 +1,23 @@
+import { makeApiRequest } from "../make-api-request";
+
+export default async function postLike(userId, clipId, commentId) {
+    const url = `http://192.168.86.195:3000/${userId}/clips/${clipId}/${commentId}/likes`;
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({})
+    };
+    const data = await makeApiRequest(url, options);
+    return data;
+}
+import { makeApiRequest } from "../make-api-request";
+
+export default async function deleteLike(userId, clipId, commentId) {
+    const url = `http://192.168.86.195:3000/${userId}/clips/${clipId}/${commentId}/likes`;
+    const options = {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({})
+    };    const data = makeApiRequest(url, options);
+    return data;
+}
