8ec62e9fc630b3573001b41fea555838dfb63d63
Erik <180784081+erhestrada@users.noreply.github.com>
2026-03-21 21:00:35 -0400

    Fix API_PATH concatenation

--- src/api/users.js
+++ src/api/users.js
@@ -2,7 +2,7 @@ import { API_PATH } from "./apiConfig.js";
 
 export async function postUser(uuid) {
     try {
-        const response = await fetch('API_PATH/postUser', {
+        const response = await fetch(API_PATH + 'postUser', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ uuid })
@@ -18,7 +18,7 @@ export async function postUser(uuid) {
 // Rename to getSignedOutUserId
 export async function getSignedOutUser(clientId) {
     try {
-        const response = await fetch(`API_PATH/users/signed-out-user?clientId=${clientId}`);
+        const response = await fetch(API_PATH + `users/signed-out-user?clientId=${clientId}`);
 
         if (!response.ok) {
             throw new Error(`HTTP error; status: ${response.status}`);
