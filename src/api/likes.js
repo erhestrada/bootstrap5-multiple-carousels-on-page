d80a94162a63ad5461db3cfa38e44acd0551d9a3
Erik <180784081+erhestrada@users.noreply.github.com>
2026-03-24 23:07:41 -0400

    Remove redundant import

--- src/api/likes.js
+++ src/api/likes.js
@@ -10,7 +10,6 @@ export async function postLike(userId, clipId, commentId) {
     const data = await makeApiRequest(url, options);
     return data;
 }
-import { makeApiRequest } from "../make-api-request";
 
 export async function deleteLike(userId, clipId, commentId) {
     const url = API_URL + `${userId}/clips/${clipId}/${commentId}/likes`;
