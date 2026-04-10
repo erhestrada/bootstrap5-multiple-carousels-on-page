ad726c7d1dd7dadaf2d7075d732bdb18270d232c
Erik <180784081+erhestrada@users.noreply.github.com>
2026-04-10 07:03:33 -0400

    Delete follows/ -- importing api functions from src/api/

--- follows/index.js
+++
@@ -1,7 +0,0 @@
-export { default as getFollows } from './get-follows';
-export { default as getKindOfFollows } from './get-kind-of-follows';
-export { default as postStreamerFollow } from './post-streamer-follow';
-export { default as deleteStreamerFollow } from './delete-streamer-follow';
-export { default as postCategoryFollow } from './post-category-follow';
-export { default as deleteCategoryFollow } from './delete-category-follow';
-export { default as patchSwapPositions } from './patch-swap-positions';
