016061948686b6c3b5ad4b2c2528bb30aec540ad
Erik <180784081+erhestrada@users.noreply.github.com>
2026-04-11 00:05:52 -0400

    Delete clips/ (clips api functions in src/api/clips.js

--- clips/index.js
+++
@@ -1,4 +0,0 @@
-export { default as getVotedOnClips } from './getVotedOnClips';
-export { default as getFavoritedClips } from './get-favorited-clips';
-export { default as getCommentedOnClips } from './getCommentedOnClips';
-export { default as getHistoryClips } from './getHistoryClips';
