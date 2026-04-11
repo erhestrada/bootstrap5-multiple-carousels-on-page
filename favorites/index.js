ae9689838af8799b614e59e3911c38eab61aeb95
Erik <180784081+erhestrada@users.noreply.github.com>
2026-04-10 22:43:58 -0400

    Delete favorites/ (favorites api functions now in src/api/)

--- favorites/index.js
+++
@@ -1,3 +0,0 @@
-export { default as postFavorite } from './post-favorite.js';
-export { default as deleteFavorite } from './delete-favorite.js';
-export { default as getFavoriteStatusOfClip } from './get-favorite-status-of-clip.js';
