d425f0af1736f8b5e0a2fa78c22986a509e3e19c
Erik <180784081+erhestrada@users.noreply.github.com>
2026-04-12 12:33:24 -0400

    Move postClip() to clips.js

--- src/api/clips.js
+++ src/api/clips.js
@@ -1,5 +1,26 @@
 import { API_URL } from "./apiConfig.js";
 
+export async function postClip(clip) {
+    try {
+        const response = await fetch('http://192.168.86.195:3000/clips', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(clip)
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const voteId = await response.json();
+        return voteId;
+    
+    } catch(error) {
+        console.error("Error posting clip to database", error);
+        throw new Error(`Error posting clip to database: ${error.message}`);
+    }
+}
+
 export async function getVotedOnClips(userId) {
     try {
         const response = await fetch(API_URL + `clips/${userId}/comments`);
