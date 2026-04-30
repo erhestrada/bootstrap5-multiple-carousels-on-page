947656445af47d9114766576bd09140668eb490b
Erik <180784081+erhestrada@users.noreply.github.com>
2026-04-29 22:06:46 -0400

    Add comment

--- getTopClips.js
+++ getTopClips.js
@@ -456,6 +456,7 @@ function makeHistoryRow(carouselName, clip, index, englishClips) {
   row.appendChild(thumbWrapper);
   row.appendChild(infoCol);
 
+  // replace carousel item
   row.addEventListener('click', () => alert('replace carousel item'));
 
   return { carouselItem: row, imageWrapper: thumbWrapper };
