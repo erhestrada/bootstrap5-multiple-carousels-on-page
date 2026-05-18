ddde48548c661cbc2ea3a15b677c43d09d2bb85b
Erik <180784081+erhestrada@users.noreply.github.com>
2026-05-18 12:31:17 -0400

    Implement getTopClipsFromBackend()

--- src/api/getTopClipsRefactor.js
+++ src/api/getTopClipsRefactor.js
@@ -1,1 +1,22 @@
-#
+import { API_URL } from "./apiConfig.js";
+
+export async function getTopClipsFromBackend(userId, clipTwitchId) {
+    try {
+        const response = await fetch(API_URL + 'clips/top', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ userId, clipTwitchId})
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error; status: ${response.status}`);
+        }
+
+        const voteId = await response.json();
+        return voteId;
+    
+    } catch(error) {
+        console.error("Error getting top clips", error);
+        throw new Error(`Error getting top clips: ${error.message}`);
+    }
+}
