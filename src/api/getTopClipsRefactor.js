18906592c4dc57b919ec08ccd24f0087e19f7f22
Erik <180784081+erhestrada@users.noreply.github.com>
2026-05-19 13:56:07 -0400

    Send getTopClips() arguments in body of api function

--- src/api/getTopClipsRefactor.js
+++ src/api/getTopClipsRefactor.js
@@ -5,8 +5,23 @@ export async function getTopClipsFromBackend(userId, clipTwitchId) {
         const response = await fetch(API_URL + 'clips/top', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify({ userId, clipTwitchId})
-        });
+            body: JSON.stringify({
+		    id: twitchId, url, 
+		    embed_url, 
+		    broadcaster_id, 
+		    broadcaster_name, 
+		    creator_id, 
+		    creator_name, 
+		    video_id, 
+		    game_id, 
+		    language, 
+		    title, 
+		    view_count, 
+		    created_at, 
+		    thumbnail_url, 
+		    duration
+		  })
+          });
 
         if (!response.ok) {
             throw new Error(`HTTP error; status: ${response.status}`);
